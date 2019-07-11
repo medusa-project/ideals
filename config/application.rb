d0030e87c669628aabea8472e99507f1ea7d10d2
Colleen Fallaw <mfall3@illinois.edu>
2019-07-11 15:50:32 -0500

    refine config

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,21 @@ module Ideals
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    attr_accessor :shibboleth_host
+
+    attr_accessor :file_mode
+
+    attr_accessor :settings
+
+    attr_accessor :storage_manager
+
+    attr_accessor :aws_signer
+
+    attr_accessor :aws_client
+
   end
 end
+
+#establish a short cut for the Application object
+Application = Ideals::Application
