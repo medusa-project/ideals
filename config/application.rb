d19bbb2d7495512bc46db0041046332441c5da4e
Colleen Fallaw <mfall3@illinois.edu>
2019-07-15 14:12:00 -0500

    refine support for login

--- config/application.rb
+++ config/application.rb
@@ -17,14 +17,15 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-class UserRole
-  ADMIN = 'admin'
-  DEPOSITOR = 'depositor'
-  GUEST = 'guest'
-  NO_DEPOSIT = 'no_deposit'
-end
-
 module Ideals
+  
+  class UserRole
+    ADMIN = 'admin'
+    DEPOSITOR = 'depositor'
+    GUEST = 'guest'
+    NO_DEPOSIT = 'no_deposit'
+  end
+
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
