fec2343f9ed8062b93e89281e642022b36cd1d54
Colleen Fallaw <mfall3@illinois.edu>
2019-11-07 12:31:29 -0600

    support nil parent group for top level collection groups

--- app/models/collection_group.rb
+++ app/models/collection_group.rb
@@ -1,14 +1,13 @@
 class CollectionGroup < ApplicationRecord
   include Breadcrumb
-  belongs_to :parent, class_name: 'CollectionGroup', foreign_key: 'parent_group_id'
+  belongs_to :parent, class_name: 'CollectionGroup', foreign_key: 'parent_group_id', optional: true
   breadcrumbs parent: nil, label: :title
   scope :academic_unit, -> { where(group_type: Ideals::CollectionGroupType::ACADEMIC_UNIT) }
   scope :top, -> { where(parent_group_id: nil) }
   scope :bottom, -> { where(child_groups.count == 0) }
   has_many :collections, dependent: :restrict_with_exception
   has_many :collection_groups, dependent: :restrict_with_exception
-
-
+  
   def label
     title
   end
