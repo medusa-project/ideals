9b4e0d25ee456e9a746dda358dfbf94b0f1e93f3
Alex Dolski <alexd@n-ary.net>
2020-01-13 16:08:45 -0600

    Replace a class name constant with a string

--- db/migrate/20190718145940_add_default_to_approval_state.rb
+++ db/migrate/20190718145940_add_default_to_approval_state.rb
@@ -1,5 +1,5 @@
 class AddDefaultToApprovalState < ActiveRecord::Migration[5.2]
   def change
-    change_column :invitees, :approval_state, :string, default: Ideals::ApprovalState::PENDING
+    change_column :invitees, :approval_state, :string, default: 'pending'
   end
 end
