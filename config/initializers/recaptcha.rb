938019d74dc425cb013d22162ead533fb1b40f04
Alex Dolski <alexd@n-ary.net>
2020-01-21 15:54:58 -0600

    Revert 07a04c9a7de17e64e2647fecbbffa1cdd89b9c4b

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -2,12 +2,9 @@
 
 require 'configuration'
 
-puts Rails.env
-=begin
 config = ::Configuration.instance
 
 Recaptcha.configure do |recaptcha|
   recaptcha.site_key   = config.recaptcha[:site_key]
   recaptcha.secret_key = config.recaptcha[:secret_key]
 end
-=end
