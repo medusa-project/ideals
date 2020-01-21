07a04c9a7de17e64e2647fecbbffa1cdd89b9c4b
Alex Dolski <alexd@n-ary.net>
2020-01-21 15:28:45 -0600

    Temporarily comment out to help debug CI

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -2,9 +2,12 @@
 
 require 'configuration'
 
+puts Rails.env
+=begin
 config = ::Configuration.instance
 
 Recaptcha.configure do |recaptcha|
   recaptcha.site_key   = config.recaptcha[:site_key]
   recaptcha.secret_key = config.recaptcha[:secret_key]
 end
+=end
