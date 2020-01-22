825a3a41febc30c7fded15c9ffe19f9cf1262c24
Alex Dolski <alexd@n-ary.net>
2020-01-22 17:42:30 -0600

    More futzing with CI

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -2,9 +2,11 @@
 
 require 'configuration'
 
-config = ::Configuration.instance
+config     = ::Configuration.instance
+site_key   = config.recaptcha[:site_key]
+secret_key = config.recaptcha[:secret_key]
 
 Recaptcha.configure do |recaptcha|
-  recaptcha.site_key   = config.recaptcha[:site_key]
-  recaptcha.secret_key = config.recaptcha[:secret_key]
+  recaptcha.site_key   = site_key
+  recaptcha.secret_key = secret_key
 end
