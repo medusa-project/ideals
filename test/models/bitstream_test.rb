f3c91014e5ec72c97e1999e5f9ec435b1832456d
Alex Dolski <alexd@n-ary.net>
2020-01-23 15:02:28 -0600

    Add some validations

--- test/models/bitstream_test.rb
+++ test/models/bitstream_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class BitstreamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @instance = bitstreams(:item1_jpg)
+    assert @instance.valid?
+  end
+
+  test "key must be present" do
+    @instance.key = nil
+    assert !@instance.valid?
+    @instance.key = ""
+    assert !@instance.valid?
+  end
+
+  test "key must be unique" do
+    @instance.update!(key:"cats")
+    assert_raises ActiveRecord::RecordInvalid do
+      Bitstream.create!(key: "cats", item: items(:item1))
+    end
+  end
+
+  test "length must be greater than or equal to zero" do
+    @instance.length = -1
+    assert !@instance.valid?
+    @instance.length = 0
+    assert @instance.valid?
+    @instance.length = 1
+    assert @instance.valid?
+  end
+
+  test "media_type must be a correct format" do
+    @instance.media_type = 'bogus'
+    assert !@instance.valid?
+  end
+
 end
