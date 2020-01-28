608cd972d92fcc6c1d48bb0cdb5dda9623fbec3d
Alex Dolski <alexd@n-ary.net>
2020-01-28 10:13:53 -0600

    Validate the name

--- app/models/registered_element.rb
+++ app/models/registered_element.rb
@@ -1,3 +1,10 @@
 class RegisteredElement < ApplicationRecord
 
+  validates_format_of :name, with: /\A[A-Za-z0-9_\-:]+\z/, allow_blank: false
+  validates_uniqueness_of :name
+
+  def to_param
+    name
+  end
+
 end
--- test/models/registered_element_test.rb
+++ test/models/registered_element_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class RegisteredElementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @instance = registered_elements(:title)
+    assert @instance.valid?
+  end
+
+  test "name must be present" do
+    @instance.name = nil
+    assert !@instance.valid?
+    @instance.name = ""
+    assert !@instance.valid?
+  end
+
+  test "name must be unique" do
+    assert_raises ActiveRecord::RecordInvalid do
+      RegisteredElement.create!(name: "title")
+    end
+  end
+
+  test "name must be of an allowed format" do
+    @instance.name = "catsCATS09:_-"
+    assert @instance.valid?
+    @instance.name = "@cats"
+    assert !@instance.valid?
+    @instance.name = "cats@"
+    assert !@instance.valid?
+    @instance.name = "c@ts"
+    assert !@instance.valid?
+  end
+
 end
