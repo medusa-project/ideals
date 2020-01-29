6840c57f8281a489d52ce37d67882770f7092457
Alex Dolski <alexd@n-ary.net>
2020-01-29 12:38:40 -0600

    Fix a broken migration

--- db/migrate/20200129182037_drop_bitstream_formats.rb
+++ db/migrate/20200129182037_drop_bitstream_formats.rb
@@ -1,5 +1,6 @@
 class DropBitstreamFormats < ActiveRecord::Migration[6.0]
   def change
+    remove_foreign_key :bitstreams, :bitstream_formats
     drop_table :bitstream_formats
     add_column :bitstreams, :media_type, :string, null: false
     remove_column :bitstreams, :bitstream_format_id
