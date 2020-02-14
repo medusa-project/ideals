e4333a33731605fa3e98b7334557026338d5a995
Alex Dolski <alexd@n-ary.net>
2020-02-14 11:48:19 -0600

    Document mandatory type casting

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+##
+# N.B.: To use this policy, an {IdentityUser} or {ShibbolethUser} instance must
+# be cast to its {User} superclass; for example:
+#
+# ```
+# authorize(user.becomes(User))
+# policy(user.becomes(User)).edit?
+# ```
+#
 class UserPolicy < ApplicationPolicy
   attr_reader :subject_user, :object_user
 
