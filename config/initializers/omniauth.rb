dc9b658df14d6984b1dda6e074b7873475e1b03a
Alex Dolski <alexd@n-ary.net>
2020-02-14 09:10:40 -0600

    Set OmniAuth's logger to Rails.logger

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,15 +1,12 @@
 shib_opts = YAML.load_file(File.join(Rails.root, 'config', 'shibboleth.yml'))[Rails.env]
 
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-
-  # provider :developer unless Rails.env.production?
-
   provider :identity,
            :fields => [:email, :name],
            :on_failed_registration => WelcomeController.action(:on_failed_registration)
-
   provider :shibboleth, shib_opts.symbolize_keys
-
 end
 
 OmniAuth.config.on_failure = Proc.new { |env|
