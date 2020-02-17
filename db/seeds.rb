4441fc4aca040c2f97103962d90206871fdef06f
Alex Dolski <alexd@n-ary.net>
2020-02-17 09:17:24 -0600

    Remove most seed data as it can interfere with importing

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,41 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-elements = {
-    title: RegisteredElement.create!(name: "title",
-                                     uri: "http://example.org/title"),
-    description: RegisteredElement.create!(name: "description",
-                                           uri: "http://example.org/description"),
-    subject: RegisteredElement.create!(name: "subject",
-                                       uri: "http://example.org/subject")
-}
-
-profile = MetadataProfile.create!(name: "Default Profile", default: true)
-profile.elements.build(registered_element: elements[:title],
-                       label: "Title",
-                       index: 0,
-                       visible: true,
-                       facetable: false,
-                       searchable: true,
-                       sortable: false,
-                       repeatable: false,
-                       required: true)
-profile.elements.build(registered_element: elements[:description],
-                       label: "Description",
-                       index: 1,
-                       visible: true,
-                       facetable: false,
-                       searchable: true,
-                       sortable: false,
-                       repeatable: true,
-                       required: false)
-profile.elements.build(registered_element: elements[:subject],
-                       label: "Subject",
-                       index: 2,
-                       visible: true,
-                       facetable: true,
-                       searchable: true,
-                       sortable: false,
-                       repeatable: true,
-                       required: false)
-profile.save!
+MetadataProfile.create!(name: "Default Profile", default: true)
