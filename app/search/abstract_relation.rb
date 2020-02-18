medusa-project/ideals

commit e013175365204fe688cf5846549f817d232c689e

Refactor AbstractRelation and subclasses

app/search/abstract_relation.rb
@@ -8,7 +8,25 @@
 #
 # TLDR: it makes interacting with Elasticsearch more like ActiveRecord.
 #
-# The normal way of obtaining an instance is via {Indexed#search}.
+# The normal way of obtaining an instance is via {Indexed#search}. That method
+# expects every searchable model to define its own subclass of this class.
+# It's possible that the subclass may not even need to override anything (i.e.
+# it may be empty). Otherwise, it may need to override {facet_elements}, if it
+# doesn't want to use only the facetable {MetadataProfileElements} in the
+# default {MetadataProfile}.
+#
+# For more extensive customizations, it can override {build_query}, which
+# basically grants it full control over the query that gets sent to
+# Elasticsearch.
+#
+# The Elasticsearch request and response communications are logged and are also
+# available via {request_json} and {response_json}.
+#
+# Why this class and not
+# [elasticsearch-model](https://github.com/elastic/elasticsearch-rails/tree/master/elasticsearch-model)?
+# Ultimately they are apples and oranges. This class fills a gap that that gem
+# doesn't, namely hiding complicated Elasticsearch queries behind a friendly
+# interface.
 #
 class AbstractRelation
 
@@ -209,7 +227,7 @@ class AbstractRelation
   end
 
   def method_missing(m, *args, &block)
-    result_instances.send(m, *args, &block)
+    @result_instances.send(m, *args, &block)
   end
 
   ##
@@ -271,25 +289,21 @@ class AbstractRelation
   end
 
   ##
-  # @return [Class]
+  # This default implementation returns all of the facetable elements in the
+  # {MetadataProfile#default default metadata profile}. Override it to return
+  # others.
   #
-  def get_class
-    raise "Subclasses must override get_class()"
-  end
-
-  ##
-  # @return [MetadataProfile] The return value of {MetadataProfile#default}.
-  #                           Subclasses can override this to use a different
-  #                           profile.
-  def metadata_profile
-    MetadataProfile.default
-  end
-
-  ##
-  # @return [String] Query that is safe to pass to Elasticsearch.
+  # @return [Enumerable<Hash<Symbol,String>>] Enumerable of Hashes with
+  #                                           `:label` and `:keyword_field`
+  #                                           keys.
   #
-  def sanitized_query
-    @query[:query].gsub(/[\[\]\(\)]/, "").gsub("/", " ")
+  def facet_elements
+    MetadataProfile.default.elements.where(facetable: true).map do |e|
+      {
+          label: e.registered_element.label,
+          keyword_field: e.registered_element.indexed_keyword_field
+      }
+    end
   end
 
   def load
@@ -300,13 +314,12 @@ class AbstractRelation
     # Assemble the response aggregations into Facets. The order of the facets
     # should be the same as the order of elements in the metadata profile.
     if @aggregations
-      metadata_profile.facetable_elements.each do |profile_element|
-        keyword_field = profile_element.registered_element.indexed_keyword_field
-        agg = @response_json['aggregations']&.find{ |a| a[0] == keyword_field }
+      facet_elements.each do |element|
+        agg = @response_json['aggregations']&.find{ |a| a[0] == element[:keyword_field] }
         if agg
           facet = Facet.new.tap do |f|
-            f.name  = profile_element.label
-            f.field = keyword_field
+            f.name  = element[:label]
+            f.field = element[:keyword_field]
           end
           agg[1]['buckets'].each do |bucket|
             facet.terms << FacetTerm.new.tap do |t|
@@ -333,7 +346,7 @@ class AbstractRelation
   end
 
   ##
-  # Builds a generic query. Subclasses should override to insert their own
+  # Builds a generic query. Subclasses can override to insert their own
   # special features.
   #
   # @return [String] JSON string.
@@ -357,7 +370,7 @@ class AbstractRelation
               else
                 j.term do
                   # Use the keyword field to get an exact match.
-                  j.set! @query[:field] + EntityElement::KEYWORD_FIELD_SUFFIX,
+                  j.set! @query[:field] + RegisteredElement::KEYWORD_FIELD_SUFFIX,
                          sanitized_query
                 end
               end
@@ -394,6 +407,20 @@ class AbstractRelation
         end
       end
 
+      # Aggregations
+      j.aggregations do
+        if @aggregations
+          facet_elements.each do |element|
+            j.set! element[:keyword_field] do
+              j.terms do
+                j.field element[:keyword_field]
+                j.size @bucket_limit
+              end
+            end
+          end
+        end
+      end
+
       # Ordering
       # Order by explicit orders, if provided; otherwise sort by the metadata
       # profile's default order, if @orders is set to true; otherwise don't
@@ -424,10 +451,21 @@ class AbstractRelation
 
   private
 
+  def get_class
+    self.class.to_s.gsub("Relation", "").constantize
+  end
+
   def get_response
     @request_json = build_query
     result = @client.query(@request_json)
     JSON.parse(result)
+  end
+
+  ##
+  # @return [String] Query that is safe to pass to Elasticsearch.
+  #
+  def sanitized_query
+    @query[:query].gsub(/[\[\]\(\)]/, "").gsub("/", " ")
   end
 
 end

app/controllers/collections_controller.rb
@@ -111,7 +111,7 @@ class CollectionsController < ApplicationController
       @start = params[:start].to_i
       @window = window_size
       @collections = Item.search.
-          collection(params[:id]).
+          filter(Item::IndexFields::COLLECTIONS, params[:id]).
           start(@start).
           limit(@window)
       @count            = @collections.count

app/controllers/units_controller.rb
@@ -87,7 +87,7 @@ class UnitsController < ApplicationController
         limit(999).
         to_a
     @collections = Collection.search.
-        primary_unit(@resource).
+        filter(Collection::IndexFields::PRIMARY_UNIT, @resource.id).
         order(RegisteredElement.sortable_field(::Configuration.instance.elements[:title])).
         limit(999).
         to_a

app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
   helper_method :current_user, :logged_in?
 
   def index
-    @item_count = Item.search.limit(0).count
+    @item_count = Item.search.aggregations(false).limit(0).count
   end
 
   def dashboard; end

app/search/collection_relation.rb
@@ -1,183 +1,21 @@
 class CollectionRelation < AbstractRelation
 
-  LOGGER = CustomLogger.new(CollectionRelation)
-
-  def initialize
-    super
-    @primary_unit = nil
-  end
-
-  ##
-  # @param primary_unit [Unit]
-  # @return [CollectionRelation] self
-  #
-  def primary_unit(primary_unit)
-    @primary_unit = primary_unit
-    self
-  end
-
   protected
 
-  def get_class
-    Collection
-  end
-
-  def load
-    return if @loaded
-
-    @response_json = get_response
-
-    # Assemble the response aggregations into Facets. The order of the facets
-    # should be the same as the order of elements in the metadata profile.
-    facet_elements.each do |element|
-      field = element[:field]
-      agg = @response_json['aggregations']&.find{ |a| a[0] == field }
-      if agg
-        facet = Facet.new.tap do |f|
-          f.name  = element[:label]
-          f.field = field
-        end
-        agg[1]['buckets'].each do |bucket|
-          facet.terms << FacetTerm.new.tap do |t|
-            t.name  = bucket['key'].to_s
-            t.label = bucket['key'].to_s
-            t.count = bucket['doc_count']
-            t.facet = facet
-          end
-        end
-        @result_facets << facet
-      end
-    end
-
-    if @response_json['hits']
-      @result_count = @response_json['hits']['total']['value']
-    else
-      @result_count = 0
-      raise IOError, "#{@response_json['error']['type']}: "\
-          "#{@response_json['error']['root_cause'][0]['reason']}"
-    end
-
-    @loaded = true
-  end
-
-  ##
-  # @return [Enumerable<Hash>] Enumerable of hashes with `label` and `field`
-  #                            keys.
-  #
   def facet_elements
-    # Ideally we would just return the result of facetable_elements() from the
-    # metadata profile. But we want to include a unit facet, for which there is
-    # no corresponding MetadataProfileElement. So instead, we build and use an
-    # array of pseudo-elements.
     elements = [
         {
             label: "Academic Unit",
-            field: "#{Collection::IndexFields::UNIT_TITLES}.keyword"
+            keyword_field: "#{Collection::IndexFields::UNIT_TITLES}.keyword"
         }
     ]
-    elements += metadata_profile.facetable_elements.map do |e|
+    elements += MetadataProfile.default.facetable_elements.map do |e|
       {
           label: e.label,
-          field: e.registered_element.indexed_keyword_field
+          keyword_field: e.registered_element.indexed_keyword_field
       }
     end
     elements
-  end
-
-  private
-
-  ##
-  # @return [String] JSON string.
-  #
-  def build_query
-    Jbuilder.encode do |j|
-      j.track_total_hits true
-      j.query do
-        j.bool do
-          # Query
-          if @query.present?
-            j.must do
-              # https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html
-              j.query_string do
-                j.query sanitized_query
-                j.default_field @query[:field]
-                j.default_operator 'AND'
-                j.lenient true
-              end
-            end
-          end
-
-          j.filter do
-            j.bool do
-              j.must do
-                j.child! do
-                  j.term do
-                    j.set! ElasticsearchIndex::StandardFields::CLASS, get_class.to_s
-                  end
-                end
-
-                @filters.each do |key_value|
-                  j.child! do
-                    if key_value[0].respond_to?(:each)
-                      j.terms do
-                        j.set! key_value[0], key_value[1]
-                      end
-                    else
-                      j.term do
-                        j.set! key_value[0], key_value[1]
-                      end
-                    end
-                  end
-                end
-
-                if @primary_unit
-                  j.child! do
-                    j.term do
-                      j.set! Collection::IndexFields::PRIMARY_UNIT, @primary_unit.id
-                    end
-                  end
-                end
-              end
-            end
-          end
-        end
-      end
-
-      # Aggregations
-      j.aggregations do
-        if @aggregations
-          facet_elements.each do |element|
-            j.set! element[:field] do
-              j.terms do
-                j.field element[:field]
-                j.size @bucket_limit
-              end
-            end
-          end
-        end
-      end
-
-      # Ordering
-      if @orders.any?
-        j.sort do
-          @orders.each do |order|
-            j.set! order[:field] do
-              j.order order[:direction]
-            end
-          end
-        end
-      end
-
-      # Start
-      if @start.present?
-        j.from @start
-      end
-
-      # Limit
-      if @limit.present?
-        j.size @limit
-      end
-    end
   end
 
 end

app/search/item_relation.rb
@@ -1,11 +1,7 @@
 class ItemRelation < AbstractRelation
 
-  LOGGER = CustomLogger.new(ItemRelation)
-
   def initialize
     super
-    @collection_id = nil
-
     # Initialize filters to include only publicly accessible items by
     # default. Clients may override where necessary.
     filter(Item::IndexFields::DISCOVERABLE, true)
@@ -13,150 +9,21 @@ class ItemRelation < AbstractRelation
     filter(Item::IndexFields::WITHDRAWN, false)
   end
 
-  ##
-  # Limits results to items in the given collection.
-  #
-  # @param collection [Collection,Integer] Instance or ID.
-  # @return [ItemRelation] self
-  #
-  def collection(collection)
-    @collection_id = collection.kind_of?(Collection) ?
-                         collection.id : collection
-    self
-  end
-
   protected
 
-  def get_class
-    Item
-  end
-
-  def metadata_profile
+  def facet_elements
     profile = nil
-    if @collection_id
-      profile = Collection.find(@collection_id)&.effective_metadata_profile
+    field = @filters.find{ |f| f[0] == Item::IndexFields::COLLECTIONS }
+    if field
+      profile = Collection.find(field[1])&.effective_metadata_profile
     end
     profile ||=  MetadataProfile.default
-    profile
-  end
-
-  ##
-  # @return [String] JSON string.
-  #
-  def build_query
-    Jbuilder.encode do |j|
-      j.track_total_hits true
-      j.query do
-        j.bool do
-          # Query
-          if @query.present?
-            j.must do
-              if !@exact_match
-                # https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html
-                j.query_string do
-                  j.query sanitized_query
-                  j.default_operator 'AND'
-                  j.lenient true
-                  j.default_field @query[:field]
-                end
-              else
-                j.term do
-                  # Use the keyword field to get an exact match.
-                  j.set! @query[:field] + EntityElement::KEYWORD_FIELD_SUFFIX,
-                         sanitized_query
-                end
-              end
-            end
-          end
-
-          j.filter do
-            j.bool do
-              j.must do
-                j.child! do
-                  j.term do
-                    j.set! ElasticsearchIndex::StandardFields::CLASS, get_class.to_s
-                  end
-                end
-
-                @filters.each do |key_value|
-                  j.child! do
-                    if key_value[0].respond_to?(:each)
-                      j.terms do
-                        j.set! key_value[0], key_value[1]
-                      end
-                    else
-                      j.term do
-                        j.set! key_value[0], key_value[1]
-                      end
-                    end
-                  end
-                end
-
-                if @collection_id
-                  j.child! do
-                    j.term do
-                      j.set! Item::IndexFields::COLLECTIONS,
-                             @collection_id
-                    end
-                  end
-                end
-              end
-            end
-          end
-        end
-      end
-
-      # Aggregations
-      j.aggregations do
-        if @aggregations
-          metadata_profile.facetable_elements.each do |profile_element|
-            keyword_field = profile_element.registered_element.indexed_keyword_field
-            j.set! keyword_field do
-              j.terms do
-                j.field keyword_field
-                j.size @bucket_limit
-              end
-            end
-          end
-        end
-      end
-
-      # Ordering
-      # Order by explicit orders, if provided; otherwise sort by the metadata
-      # profile's default order, if @orders is set to true; otherwise don't
-      # sort.
-      if @orders.respond_to?(:any?) and @orders.any?
-        j.sort do
-          @orders.each do |order|
-            j.set! order[:field] do
-              j.order order[:direction]
-              j.unmapped_type 'keyword'
-            end
-          end
-        end
-      elsif @orders
-=begin
-           el = metadata_profile.default_sortable_element
-           if el
-             j.sort do
-               j.set! el.indexed_sort_field do
-                 j.order 'asc'
-                 j.unmapped_type 'keyword'
-               end
-             end
-           end
-=end
-      end
-
-      # Start
-      if @start.present?
-        j.from @start
-      end
-
-      # Limit
-      if @limit.present?
-        j.size @limit
-      end
+    profile.elements.where(facetable: true).map do |e|
+      {
+          label: e.registered_element.label,
+          keyword_field: e.registered_element.indexed_keyword_field
+      }
     end
   end
+
 end

app/search/unit_relation.rb
@@ -32,12 +32,6 @@ class UnitRelation < AbstractRelation
 
   protected
 
-  def get_class
-    Unit
-  end
-
-  private
-
   ##
   # @return [String] JSON string.
   #

app/search/user_relation.rb
@@ -1,11 +1,3 @@
 class UserRelation < AbstractRelation
 
-  LOGGER = CustomLogger.new(UserRelation)
-
-  protected
-
-  def get_class
-    User
-  end
-
 end

commit 18c7031f2094785e6bc3187c8e7b55cabda23fb1

Fix a regression from e013175365204fe688cf5846549f817d232c689e

app/search/abstract_relation.rb
@@ -387,7 +387,7 @@ class AbstractRelation
                 end
 
                 @filters.each do |key_value|
-                  if key_value[1].present?
+                  unless key_value[1].nil?
                     j.child! do
                       if key_value[0].respond_to?(:each)
                         j.terms do