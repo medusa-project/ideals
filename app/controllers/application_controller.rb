cda3d270a914b37d932cb6f41f9d3b5d0a034d10
Alex Dolski <alexd@n-ary.net>
2020-02-20 15:20:39 -0600

    Add :window to permitted results params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -69,7 +69,7 @@ class ApplicationController < ActionController::Base
   end
 
   def results_params
-    params.permit(:q, :start, fq: [])
+    params.permit(:q, :start, :window, fq: [])
   end
 
   ##
