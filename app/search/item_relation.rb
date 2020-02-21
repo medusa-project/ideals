0900bcf7671cdee95472cc92346331576c4a3170
Alex Dolski <alexd@n-ary.net>
2020-02-21 12:01:00 -0600

    facet_elements() includes a unit facet

--- app/search/item_relation.rb
+++ app/search/item_relation.rb
@@ -12,18 +12,30 @@ class ItemRelation < AbstractRelation
   protected
 
   def facet_elements
+    elements = [
+        {
+            label: "Academic Unit",
+            keyword_field: Item::IndexFields::UNIT_TITLES
+        }
+    ]
+    elements += metadata_profile.elements.where(facetable: true).map do |e|
+      {
+          label: e.label,
+          keyword_field: e.registered_element.indexed_keyword_field
+      }
+    end
+    elements
+  end
+
+  private
+
+  def metadata_profile
     profile = nil
     field = @filters.find{ |f| f[0] == Item::IndexFields::COLLECTIONS }
     if field
       profile = Collection.find(field[1])&.effective_metadata_profile
     end
-    profile ||=  MetadataProfile.default
-    profile.elements.where(facetable: true).map do |e|
-      {
-          label: e.registered_element.label,
-          keyword_field: e.registered_element.indexed_keyword_field
-      }
-    end
+    profile || MetadataProfile.default
   end
 
 end
