c30dc7d6724d61ca969d04670b21287186e9d74c
Alex Dolski <alexd@n-ary.net>
2020-02-26 15:23:46 -0600

    Update submission profile elements

--- app/util/ideals_seeder.rb
+++ app/util/ideals_seeder.rb
@@ -50,26 +50,104 @@ class IdealsSeeder
   end
 
   def seed_submission_profiles
+    # This list of elements copied from:
+    # https://uofi.app.box.com/notes/593479281190
     profile = SubmissionProfile.create!(name: "Default Submission Profile",
                                         default: true)
     profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:title"),
                            index: 0,
-                           repeatable: false,
+                           repeatable: true,
                            required: true)
     profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:date:issued"),
                            index: 1,
-                           repeatable: false,
-                           required: false)
-    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:subject"),
+                           repeatable: true,
+                           required: true)
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:type"),
                            index: 2,
                            repeatable: true,
-                           required: false)
-    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:identifier:uri"),
+                           required: true) # TODO: checkbox list controlled by common types
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:subject"),
                            index: 3,
+                           repeatable: true,
+                           required: true)
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:creator"),
+                           index: 4,
+                           repeatable: true,
+                           required: false)
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:contributor"),
+                           index: 5,
+                           repeatable: true,
+                           required: false)
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:description:abstract"),
+                           index: 6,
                            repeatable: false,
                            required: false)
-    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:type"),
-                           index: 4,
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:coverage:spatial"),
+                           index: 7,
+                           repeatable: true,
+                           required: false)
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:type:genre"),
+                           index: 8,
+                           repeatable: true,
+                           required: false)
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:language"),
+                           index: 9,
+                           repeatable: false,
+                           required: false) # TODO: dropdown controlled by ISO languages
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:identifier:bibliographicCitation"),
+                           index: 10,
+                           repeatable: false,
+                           required: false)
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:publisher"),
+                           index: 11,
+                           repeatable: false,
+                           required: false)
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:relation:ispartof"),
+                           index: 12,
+                           repeatable: true,
+                           required: false)
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:description:sponsorship"),
+                           index: 13,
+                           repeatable: true,
+                           required: false)
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:rights"),
+                           index: 14,
+                           repeatable: true,
+                           required: false)
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:identifier"),
+                           index: 15,
+                           repeatable: true,
+                           required: false)
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("thesis:degree:name"),
+                           index: 16,
+                           repeatable: false,
+                           required: false)
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("thesis:degree:level"),
+                           index: 17,
+                           repeatable: false,
+                           required: false) # TODO: controlled dropdown
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:contributor:committeeChair"),
+                           index: 18,
+                           repeatable: true,
+                           required: false)
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("dc:contributor:advisor"),
+                           index: 19,
+                           repeatable: true,
+                           required: false)
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("thesis:degree:grantor"),
+                           index: 20,
+                           repeatable: false,
+                           required: false)
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("thesis:degree:discipline"),
+                           index: 21,
+                           repeatable: false,
+                           required: false)
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("thesis:degree:department"),
+                           index: 22,
+                           repeatable: false,
+                           required: false)
+    profile.elements.build(registered_element: RegisteredElement.find_by_name("thesis:degree:program"),
+                           index: 23,
                            repeatable: false,
                            required: false)
     profile.save!
