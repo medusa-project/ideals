ed01c6c4ed198dc41ddc01c8495dd1bbceeebfa8
Alex Dolski <alexd@n-ary.net>
2020-03-12 12:29:41 -0500

    Remove unit_tree_checkboxes()

--- app/helpers/units_helper.rb
+++ app/helpers/units_helper.rb
@@ -32,46 +32,6 @@ module UnitsHelper
   end
 
   ##
-  # @param selected_unit_ids [Enumerable<Integer>] IDs of units to check.
-  # @param parent_unit [Unit] Not part of the public contract--ignore.
-  # @param io [StringIO]      Not part of the public contract--ignore.
-  # @param level [Integer]    Not part of the public contract--ignore.
-  # @return [String] HTML checkboxes.
-  #
-  def unit_tree_checkboxes(selected_unit_ids = [],
-                           parent_unit = nil,
-                           io = StringIO.new,
-                           level = 0)
-    if level == 0
-      # Ensures that an empty array is sent when no boxes are checked.
-      io << hidden_field_tag("collection[unit_ids][]")
-    end
-    units = Unit.search.
-        order("#{Unit::IndexFields::TITLE}.sort").
-        limit(999)
-    if parent_unit
-      units.parent_unit(parent_unit)
-    else
-      units.include_children(false)
-    end
-    units.to_a.each do |unit|
-      indent = "&nbsp;&nbsp;&nbsp;&nbsp;" * level
-      arrow  = (level > 0) ? raw("&#8627; ") : ""
-      io << "<div class=\"form-check\">"
-      io << check_box_tag("collection[unit_ids][]", unit.id,
-                          selected_unit_ids.include?(unit.id),
-                          { id: "collection_unit_#{unit.id}",
-                            class: "form-check-input" })
-      io << label_tag("collection_unit_#{unit.id}",
-                      raw(indent + arrow + unit.title),
-                      class: "form-check-label")
-      io << "</div>"
-      unit_tree_checkboxes(selected_unit_ids, unit, io, level + 1)
-    end
-    raw(io.string)
-  end
-
-  ##
   # @param include_blank [Boolean]      Whether to include a blank entry at the
   #                                     top, typically for root units.
   # @param parent_unit [Unit]           Not part of the public contract--ignore.
