78af6a76fffb5ada963714f30606281fd64a4901
Alex Dolski <alexd@n-ary.net>
2020-04-21 09:52:46 -0500

    Remove Array

--- app/models/resource_type.rb
+++ app/models/resource_type.rb
@@ -4,9 +4,10 @@ class ResourceType
   ITEM = 2
   COLLECTION = 3
   UNIT = 4
-  HASH = {0 => 'Bitstream', 2 => 'Item', 3 => 'Collection', 4 => 'Unit'}
-  Array = [ResourceTypeInfo.new('Bitstream', 0),
-           ResourceTypeInfo.new('Item', 2),
-           ResourceTypeInfo.new('Collection', 3),
-           ResourceTypeInfo.new('Unit', 4)]
+  HASH = {
+      BITSTREAM => 'Bitstream',
+      ITEM => 'Item',
+      COLLECTION => 'Collection',
+      UNIT => 'Unit'
+  }
 end
