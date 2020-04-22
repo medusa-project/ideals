7e66ccbb96b5bc33e37fe12ca682dd2eed1dc6ad
Alex Dolski <alexd@n-ary.net>
2020-04-22 08:43:11 -0500

    index() rescues Elasticsearch errors

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,23 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
-  helper_method :current_user, :logged_in?
 
+  ##
+  # Responds to `GET /`
+  #
   def index
     @item_count = policy_scope(Item.search.aggregations(false).limit(0),
                                policy_scope_class: ItemPolicy::Scope).count
-    @user = current_user
-    @submissions_in_progress = @user.submitted_items.
-        where(submitting: true).
-        order(updated_at: :desc) if @user
+    user = current_user
+    if user
+      @submissions_in_progress = user.submitted_items.
+          where(submitting: true).
+          order(updated_at: :desc)
+    end
+  rescue Errno::ECONNREFUSED
+    # Elasticsearch is inaccessible. This is a major problem, but not in this
+    # view specifically. Other views that depend more strongly on ES will not
+    # rescue this error.
   end
 
   def login_choice; end
