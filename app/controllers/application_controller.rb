medusa-project/ideals

commit 61cd0c54736880a32eeac7e3a8d627568a68da16

error_occurred() does not send an email in development

app/controllers/application_controller.rb
@@ -76,14 +76,15 @@ class ApplicationController < ActionController::Base
 
       exception_string += "\nCurrent User: #{current_user.name} | #{current_user.email}" if current_user
 
-      notification = IdealsMailer.error(exception_string)
-      notification.deliver_now
-      respond_to do |format|
-        format.html { render "errors/error500", status: :internal_server_error }
-        format.json { render nothing: true, status: :internal_server_error }
-        format.xml { render xml: {status: 500}.to_xml }
+      unless Rails.env.development?
+        notification = IdealsMailer.error(exception_string)
+        notification.deliver_now
+        respond_to do |format|
+          format.html { render "errors/error500", status: :internal_server_error }
+          format.json { render nothing: true, status: :internal_server_error }
+          format.xml { render xml: {status: 500}.to_xml }
+        end
       end
-
     end
   end
 

commit 798b5ab47a883a8efd1bc2d5e71e6a221257c508

Fix a regression from 61cd0c54736880a32eeac7e3a8d627568a68da16

app/controllers/application_controller.rb
@@ -74,16 +74,15 @@ class ApplicationController < ActionController::Base
 
       Rails.logger.warn(exception_string)
 
-      exception_string += "\nCurrent User: #{current_user.name} | #{current_user.email}" if current_user
-
       unless Rails.env.development?
+        exception_string += "\nCurrent User: #{current_user.name} | #{current_user.email}" if current_user
         notification = IdealsMailer.error(exception_string)
         notification.deliver_now
-        respond_to do |format|
-          format.html { render "errors/error500", status: :internal_server_error }
-          format.json { render nothing: true, status: :internal_server_error }
-          format.xml { render xml: {status: 500}.to_xml }
-        end
+      end
+      respond_to do |format|
+        format.html { render "errors/error500", status: :internal_server_error }
+        format.json { render nothing: true, status: :internal_server_error }
+        format.xml { render xml: {status: 500}.to_xml }
       end
     end
   end