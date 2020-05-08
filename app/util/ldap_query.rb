medusa-project/ideals

commit b0f9ef2d09bf89e2091adba9ce01cb501bb5b662

Address Ruby 2.7 deprecations

app/util/ldap_query.rb
@@ -35,7 +35,7 @@ class LdapQuery
   end
 
   def ldap_url(group, net_id)
-    "https://quest.library.illinois.edu/directory/ad/#{net_id}/ismemberof/#{URI.encode(group)}"
+    "https://quest.library.illinois.edu/directory/ad/#{net_id}/ismemberof/#{CGI.escape(group)}"
   end
 
   def self.ldap_cache_key(net_id)

test/controllers/bitstreams_controller_test.rb
@@ -52,7 +52,7 @@ class BitstreamsControllerTest < ActionDispatch::IntegrationTest
   test "create() returns HTTP 400 for illegal arguments" do
     skip # TODO: figure out how to POST raw data, i.e. not multipart/form-data
     log_in_as(users(:admin))
-    post item_bitstreams_path(items(:item1)), {}
+    post item_bitstreams_path(items(:item1))
     assert_response :bad_request
   end
 

test/controllers/collections_controller_test.rb
@@ -25,71 +25,67 @@ class CollectionsControllerTest < ActionDispatch::IntegrationTest
   # create()
 
   test "create() redirects to login page for logged-out users" do
-    post collections_path, {}
+    post collections_path
     assert_redirected_to login_path
   end
 
   test "create() returns HTTP 403 for unauthorized users" do
     log_in_as(users(:norights))
-    post collections_path, {
-        xhr: true,
-        params: {
-            collection: {
-                primary_unit_id: units(:unit1).id
-            },
-            elements: {
-                title: "New Collection"
-            }
-        }
-    }
+    post collections_path,
+         xhr: true,
+         params: {
+             collection: {
+                 primary_unit_id: units(:unit1).id
+             },
+             elements: {
+                 title: "New Collection"
+             }
+         }
     assert_response :forbidden
   end
 
   test "create() returns HTTP 200 for authorized users" do
     log_in_as(users(:admin))
-    post collections_path, {
-        xhr: true,
-        params: {
-            collection: {
-                manager_id: users(:admin).id,
-                primary_unit_id: units(:unit1).id
-            },
-            elements: {
-                title: "New Collection"
-            }
-        }
-    }
+    post collections_path,
+         xhr: true,
+         params: {
+             collection: {
+                 manager_id: users(:admin).id,
+                 primary_unit_id: units(:unit1).id
+             },
+             elements: {
+                 title: "New Collection"
+             }
+         }
     assert_response :ok
   end
 
   test "create() creates a collection" do
     log_in_as(users(:admin))
     assert_difference "Collection.count" do
-      post collections_path, {
-          xhr: true,
-          params: {
-              collection: {
-                  manager_id: users(:admin).id,
-                  primary_unit_id: units(:unit1).id
-              },
-              elements: {
-                  title: "New Collection"
-              }
-          }
-      }
+      post collections_path,
+           xhr: true,
+           params: {
+               collection: {
+                   manager_id: users(:admin).id,
+                   primary_unit_id: units(:unit1).id
+               },
+               elements: {
+                   title: "New Collection"
+               }
+           }
     end
   end
 
   test "create() returns HTTP 400 for illegal arguments" do
     log_in_as(users(:admin))
-    post collections_path, {
-        xhr: true,
-        params: {
-            collection: {
-                primary_unit_id: 99999
-            }
-        }
-    }
+    post collections_path,
+         xhr: true,
+         params: {
+             collection: {
+                 primary_unit_id: 99999
+             }
+         }
     assert_response :bad_request
   end
 
@@ -119,7 +115,7 @@ class CollectionsControllerTest < ActionDispatch::IntegrationTest
     log_in_as(users(:admin))
     collection = collections(:collection1)
     primary_unit = collection.primary_unit
-    delete "/collections/#{collection.id}"
+    delete collection_path(collection)
     assert_redirected_to primary_unit
   end
 
@@ -133,28 +129,28 @@ class CollectionsControllerTest < ActionDispatch::IntegrationTest
 
   test "edit_access() redirects to login page for logged-out users" do
     collection = collections(:collection1)
-    get "/collections/#{collection.id}/edit-access", xhr: true
+    get collection_edit_access_path(collection), xhr: true
     assert_redirected_to login_path
   end
 
   test "edit_access() returns HTTP 403 for unauthorized users" do
     log_in_as(users(:norights))
     collection = collections(:collection1)
-    get "/collections/#{collection.id}/edit-access", xhr: true
+    get collection_edit_access_path(collection), xhr: true
     assert_response :forbidden
   end
 
   test "edit_access() returns HTTP 404 for non-XHR requests" do
     log_in_as(users(:admin))
     collection = collections(:collection1)
-    get "/collections/#{collection.id}/edit-access"
+    get collection_edit_access_path(collection)
     assert_response :not_found
   end
 
   test "edit_access() returns HTTP 200 for XHR requests" do
     log_in_as(users(:admin))
     collection = collections(:collection1)
-    get "/collections/#{collection.id}/edit-access", xhr: true
+    get collection_edit_access_path(collection), xhr: true
     assert_response :ok
   end
 
@@ -162,28 +158,28 @@ class CollectionsControllerTest < ActionDispatch::IntegrationTest
 
   test "edit_collection_membership() redirects to login page for logged-out users" do
     collection = collections(:collection1)
-    get "/collections/#{collection.id}/edit-collection-membership", xhr: true
+    get collection_edit_collection_membership_path(collection), xhr: true
     assert_redirected_to login_path
   end
 
   test "edit_collection_membership() returns HTTP 403 for unauthorized users" do
     log_in_as(users(:norights))
     collection = collections(:collection1)
-    get "/collections/#{collection.id}/edit-collection-membership", xhr: true
+    get collection_edit_collection_membership_path(collection), xhr: true
     assert_response :forbidden
   end
 
   test "edit_collection_membership() returns HTTP 404 for non-XHR requests" do
     log_in_as(users(:admin))
     collection = collections(:collection1)
-    get "/collections/#{collection.id}/edit-collection-membership"
+    get collection_edit_collection_membership_path(collection)
     assert_response :not_found
   end
 
   test "edit_collection_membership() returns HTTP 200 for XHR requests" do
     log_in_as(users(:admin))
     collection = collections(:collection1)
-    get "/collections/#{collection.id}/edit-collection-membership", xhr: true
+    get collection_edit_collection_membership_path(collection), xhr: true
     assert_response :ok
   end
 
@@ -191,14 +187,14 @@ class CollectionsControllerTest < ActionDispatch::IntegrationTest
 
   test "edit_properties() redirects to login page for logged-out users" do
     collection = collections(:collection1)
-    get "/collections/#{collection.id}/edit-properties", xhr: true
+    get collection_edit_properties_path(collection), xhr: true
     assert_redirected_to login_path
   end
 
   test "edit_properties() returns HTTP 403 for unauthorized users" do
     log_in_as(users(:norights))
     collection = collections(:collection1)
-    get "/collections/#{collection.id}/edit-properties", xhr: true
+    get collection_edit_properties_path(collection), xhr: true
     assert_response :forbidden
   end
 
@@ -220,28 +216,28 @@ class CollectionsControllerTest < ActionDispatch::IntegrationTest
 
   test "edit_unit_membership() redirects to login page for logged-out users" do
     collection = collections(:collection1)
-    get "/collections/#{collection.id}/edit-unit-membership", xhr: true
+    get collection_edit_unit_membership_path(collection), xhr: true
     assert_redirected_to login_path
   end
 
   test "edit_unit_membership() returns HTTP 403 for unauthorized users" do
     log_in_as(users(:norights))
     collection = collections(:collection1)
-    get "/collections/#{collection.id}/edit-unit-membership", xhr: true
+    get collection_edit_unit_membership_path(collection), xhr: true
     assert_response :forbidden
   end
 
   test "edit_unit_membership() returns HTTP 200 for non-XHR requests" do
     log_in_as(users(:admin))
     collection = collections(:collection1)
-    get "/collections/#{collection.id}/edit-unit-membership"
+    get collection_edit_unit_membership_path(collection)
     assert_response :not_found
   end
 
   test "edit_unit_membership() returns HTTP 200 for XHR requests" do
     log_in_as(users(:admin))
     collection = collections(:collection1)
-    get "/collections/#{collection.id}/edit-unit-membership", xhr: true
+    get collection_edit_unit_membership_path(collection), xhr: true
     assert_response :ok
   end
 
@@ -282,31 +278,30 @@ class CollectionsControllerTest < ActionDispatch::IntegrationTest
 
   test "update() redirects to login page for logged-out users" do
     collection = collections(:collection1)
-    patch "/collections/#{collection.id}", {}
+    patch collection_path(collection)
     assert_redirected_to login_path
   end
 
   test "update() returns HTTP 403 for unauthorized users" do
     log_in_as(users(:norights))
     collection = collections(:collection1)
-    patch "/collections/#{collection.id}", {}
+    patch collection_path(collection)
     assert_response :forbidden
   end
 
   test "update() updates a collection" do
     log_in_as(users(:admin))
     collection = collections(:collection1)
-    patch "/collections/#{collection.id}", {
-        xhr: true,
-        params: {
-            collection: {
-                primary_unit_id: collection.primary_unit_id
-            },
-            elements: {
-                "dc:description": "New description"
-            }
-        }
-    }
+    patch collection_path(collection),
+          xhr: true,
+          params: {
+              collection: {
+                  primary_unit_id: collection.primary_unit_id
+              },
+              elements: {
+                  "dc:description": "New description"
+              }
+          }
     collection.reload
     assert_equal "New description", collection.description
   end
@@ -314,34 +309,32 @@ class CollectionsControllerTest < ActionDispatch::IntegrationTest
   test "update() returns HTTP 200" do
     log_in_as(users(:admin))
     collection = collections(:collection1)
-    patch "/collections/#{collection.id}", {
-        xhr: true,
-        params: {
-            collection: {
-                managing_user_ids: [ users(:admin).id ]
-            }
-        }
-    }
+    patch collection_path(collection),
+          xhr: true,
+          params: {
+              collection: {
+                  managing_user_ids: [ users(:admin).id ]
+              }
+          }
     assert_response :ok
   end
 
   test "update() returns HTTP 400 for illegal arguments" do
     log_in_as(users(:admin))
     collection = collections(:collection1)
-    patch "/collections/#{collection.id}", {
-        xhr: true,
-        params: {
-            collection: {
-                primary_unit_id: 99999
-            }
-        }
-    }
+    patch collection_path(collection),
+          xhr: true,
+          params: {
+              collection: {
+                  primary_unit_id: 99999
+              }
+          }
     assert_response :bad_request
   end
 
   test "update() returns HTTP 404 for nonexistent collections" do
     log_in_as(users(:admin))
-    patch "/collections/bogus", {}
+    patch "/collections/bogus"
     assert_response :not_found
   end
 

test/controllers/invitees_controller_test.rb
@@ -6,14 +6,14 @@ class InviteesControllerTest < ActionDispatch::IntegrationTest
 
   test "approve() redirects to login page for logged-out users" do
     invitee = invitees(:pending)
-    patch invitee_approve_path(invitee), {}
+    patch invitee_approve_path(invitee)
     assert_redirected_to login_path
   end
 
   test "approve() returns HTTP 403 for unauthorized users" do
     log_in_as(users(:norights))
     invitee = invitees(:pending)
-    patch invitee_approve_path(invitee), {}
+    patch invitee_approve_path(invitee)
     assert_response :forbidden
   end
 
@@ -23,7 +23,7 @@ class InviteesControllerTest < ActionDispatch::IntegrationTest
     assert !invitee.approved?
 
     assert_emails 1 do
-      patch invitee_approve_path(invitee), {}
+      patch invitee_approve_path(invitee)
       invitee.reload
       assert invitee.approved?
     end
@@ -32,7 +32,7 @@ class InviteesControllerTest < ActionDispatch::IntegrationTest
   test "approve() sets the flash and redirects upon success" do
     log_in_as(users(:admin))
     invitee = invitees(:pending)
-    patch invitee_approve_path(invitee), {}
+    patch invitee_approve_path(invitee)
     assert_redirected_to invitees_path
     assert flash['success'].start_with?("Invitee #{invitee.email} has been approved")
   end
@@ -42,15 +42,14 @@ class InviteesControllerTest < ActionDispatch::IntegrationTest
   test "create() returns HTTP 400 for illegal arguments" do
     log_in_as(users(:admin))
 
-    post invitees_path, {
-        xhr: true,
-        params: {
-            invitee: {
-                email: "",
-                note: "This is a new invitee"
-            }
-        }
-    }
+    post invitees_path,
+         xhr: true,
+         params: {
+             invitee: {
+                 email: "",
+                 note: "This is a new invitee"
+             }
+         }
     assert_response :bad_request
   end
 
@@ -62,15 +61,14 @@ class InviteesControllerTest < ActionDispatch::IntegrationTest
     assert_nil Invitee.find_by_email(email)
 
     assert_emails 1 do
-      post invitees_path, {
-          xhr: true,
-          params: {
-              invitee: {
-                  email: email,
-                  note: "This is a new invitee"
-              }
-          }
-      }
+      post invitees_path,
+           xhr: true,
+           params: {
+               invitee: {
+                   email: email,
+                   note: "This is a new invitee"
+               }
+           }
       invitee = Invitee.find_by_email(email)
       assert_equal ApprovalState::APPROVED, invitee.approval_state
     end
@@ -79,44 +77,41 @@ class InviteesControllerTest < ActionDispatch::IntegrationTest
   test "create() sets the flash if all arguments are valid" do
     log_in_as(users(:admin))
 
-    post invitees_path, {
-        xhr: true,
-        params: {
-            invitee: {
-                email: "new@example.edu",
-                note: "This is a new invitee"
-            }
-        }
-    }
+    post invitees_path,
+         xhr: true,
+         params: {
+             invitee: {
+                 email: "new@example.edu",
+                 note: "This is a new invitee"
+             }
+         }
     assert flash['success'].include?("An invitation has been sent")
   end
 
   test "create() returns HTTP 200 if all arguments are valid" do
     log_in_as(users(:admin))
 
-    post invitees_path, {
-        xhr: true,
-        params: {
-            invitee: {
-                email: "new@example.edu",
-                note: "This is a new invitee"
-            }
-        }
-    }
+    post invitees_path,
+         xhr: true,
+         params: {
+             invitee: {
+                 email: "new@example.edu",
+                 note: "This is a new invitee"
+             }
+         }
     assert_response :ok
   end
 
   # create_unsolicited()
 
   test "create_unsolicited() redirects for illegal arguments" do
-    post create_unsolicited_invitees_path, {
-        params: {
-            invitee: {
-                email: "",
-                note: "This is a new invitee"
-            }
-        }
-    }
+    post create_unsolicited_invitees_path,
+         params: {
+             invitee: {
+                 email: "",
+                 note: "This is a new invitee"
+             }
+         }
     assert_redirected_to new_invitee_path
   end
 
@@ -126,28 +121,26 @@ class InviteesControllerTest < ActionDispatch::IntegrationTest
     assert_nil Invitee.find_by_email(email)
 
     assert_emails 2 do
-      post create_unsolicited_invitees_path, {
-          params: {
-              invitee: {
-                  email: email,
-                  note: "This is a new invitee"
-              }
-          }
-      }
+      post create_unsolicited_invitees_path,
+           params: {
+               invitee: {
+                   email: email,
+                   note: "This is a new invitee"
+               }
+           }
     end
     invitee = Invitee.find_by_email(email)
     assert_equal ApprovalState::PENDING, invitee.approval_state
   end
 
   test "create_unsolicited() sets the flash and redirects if all arguments are valid" do
-    post create_unsolicited_invitees_path, {
-        params: {
-            invitee: {
-                email: "new@example.edu",
-                note: "This is a new invitee"
-            }
-        }
-    }
+    post create_unsolicited_invitees_path,
+         params: {
+             invitee: {
+                 email: "new@example.edu",
+                 note: "This is a new invitee"
+             }
+         }
     assert flash['success'].start_with?("Thanks for requesting")
     assert_redirected_to root_url
   end
@@ -229,14 +222,14 @@ class InviteesControllerTest < ActionDispatch::IntegrationTest
 
   test "reject() redirects to login page for logged-out users" do
     invitee = invitees(:pending)
-    patch invitee_reject_path(invitee), {}
+    patch invitee_reject_path(invitee)
     assert_redirected_to login_path
   end
 
   test "reject() returns HTTP 403 for unauthorized users" do
     log_in_as(users(:norights))
     invitee = invitees(:pending)
-    patch invitee_reject_path(invitee), {}
+    patch invitee_reject_path(invitee)
     assert_response :forbidden
   end
 
@@ -246,7 +239,7 @@ class InviteesControllerTest < ActionDispatch::IntegrationTest
     assert !invitee.rejected?
 
     assert_emails 1 do
-      patch invitee_reject_path(invitee), {}
+      patch invitee_reject_path(invitee)
       invitee.reload
       assert invitee.rejected?
     end
@@ -255,7 +248,7 @@ class InviteesControllerTest < ActionDispatch::IntegrationTest
   test "reject() sets the flash and redirects upon success" do
     log_in_as(users(:admin))
     invitee = invitees(:pending)
-    patch invitee_reject_path(invitee), {}
+    patch invitee_reject_path(invitee)
     assert_redirected_to invitees_path
     assert flash['success'].start_with?("Invitee #{invitee.email} has been rejected")
   end
@@ -264,21 +257,21 @@ class InviteesControllerTest < ActionDispatch::IntegrationTest
 
   test "resend_email() redirects to login page for logged-out users" do
     invitee = invitees(:pending)
-    patch invitee_resend_email_path(invitee), {}
+    patch invitee_resend_email_path(invitee)
     assert_redirected_to login_path
   end
 
   test "resend_email() returns HTTP 403 for unauthorized users" do
     log_in_as(users(:norights))
     invitee = invitees(:pending)
-    patch invitee_resend_email_path(invitee), {}
+    patch invitee_resend_email_path(invitee)
     assert_response :forbidden
   end
 
   test "resend_email() redirects to the invitees path upon success" do
     log_in_as(users(:admin))
     invitee = invitees(:approved)
-    patch invitee_resend_email_path(invitee), {}
+    patch invitee_resend_email_path(invitee)
     assert_redirected_to invitees_path
   end
 
@@ -286,7 +279,7 @@ class InviteesControllerTest < ActionDispatch::IntegrationTest
     log_in_as(users(:admin))
     invitee = invitees(:approved)
     assert_emails 1 do
-      patch invitee_resend_email_path(invitee), {}
+      patch invitee_resend_email_path(invitee)
     end
   end
 

test/controllers/items_controller_test.rb
@@ -48,14 +48,14 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "edit_membership() redirects to login page for logged-out users" do
     item = items(:item1)
-    get "/items/#{item.id}/edit-membership", xhr: true
+    get item_edit_membership_path(item), xhr: true
     assert_redirected_to login_path
   end
 
   test "edit_membership() returns HTTP 403 for unauthorized users" do
     log_in_as(users(:norights))
     item = items(:item1)
-    get "/items/#{item.id}/edit-membership", xhr: true
+    get item_edit_membership_path(item), xhr: true
     assert_response :forbidden
   end
 
@@ -77,14 +77,14 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "edit_metadata() redirects to login page for logged-out users" do
     item = items(:item1)
-    get "/items/#{item.id}/edit-metadata", xhr: true
+    get item_edit_metadata_path(item), xhr: true
     assert_redirected_to login_path
   end
 
   test "edit_metadata() returns HTTP 403 for unauthorized users" do
     log_in_as(users(:norights))
     item = items(:item1)
-    get "/items/#{item.id}/edit-metadata", xhr: true
+    get item_edit_metadata_path(item), xhr: true
     assert_response :forbidden
   end
 
@@ -106,14 +106,14 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "edit_properties() redirects to login page for logged-out users" do
     item = items(:item1)
-    get "/items/#{item.id}/edit-properties", xhr: true
+    get item_edit_properties_path(item), xhr: true
     assert_redirected_to login_path
   end
 
   test "edit_properties() returns HTTP 403 for unauthorized users" do
     log_in_as(users(:norights))
     item = items(:item1)
-    get "/items/#{item.id}/edit-properties", xhr: true
+    get item_edit_properties_path(item), xhr: true
     assert_response :forbidden
   end
 

commit afbabca22b55c68cbe6432d60b5990976d9ee1d4

Fix a regression from b0f9ef2d09bf89e2091adba9ce01cb501bb5b662

app/util/ldap_query.rb
@@ -34,8 +34,9 @@ class LdapQuery
     end
   end
 
-  def ldap_url(group, net_id)
-    "https://quest.library.illinois.edu/directory/ad/#{net_id}/ismemberof/#{CGI.escape(group)}"
+  def ldap_url(group, netid)
+    group = group.gsub(" ", "%20") # URI.encode() does this but is deprecated
+    "https://quest.library.illinois.edu/directory/ad/#{netid}/ismemberof/#{group}"
   end
 
   def self.ldap_cache_key(net_id)