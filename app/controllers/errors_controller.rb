10154f4da93c4d8f5ef8a4606db0c302b48030cf
Alex Dolski <alexd@n-ary.net>
2020-05-26 09:02:19 -0500

    Remove error500()

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -7,11 +7,4 @@ class ErrorsController < ApplicationController
       format.all { render nothing: true, status: :not_found }
     end
   end
-
-  def error500
-    respond_to do |format|
-      format.html { render "errors/error500", status: :internal_server_error }
-      format.all { render nothing: true, status: :internal_server_error }
-    end
-  end
 end
