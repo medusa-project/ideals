medusa-project/ideals

commit c27a0b80a799c8546322baea3b527736b4a42cab

Rename Item.in_archive?() to exists_in_medusa?() and fix a couple of broken tests

app/models/item.rb
@@ -103,7 +103,7 @@ class Item < ApplicationRecord
   after_save :assign_handle, if: -> {
     !submitting && handle.nil? && !IdealsImporter.instance.running? }
   after_save :ingest_into_medusa, if: -> {
-    !submitting && handle.present? && !IdealsImporter.instance.running? }
+    !submitting && handle.present? && !exists_in_medusa? && !IdealsImporter.instance.running? }
   before_destroy :restrict_in_archive_deletion
 
   ##
@@ -246,12 +246,12 @@ class Item < ApplicationRecord
   # @return [Boolean] Whether all of the instance's associated Bitstreams have
   #         been ingested into Medusa.
   #
-  def in_archive?
-    self.bitstreams.where(medusa_uuid: nil).count > 0
+  def exists_in_medusa?
+    self.bitstreams.where.not(medusa_uuid: nil).count > 0
   end
 
   ##
-  # Uploads all of the instance's associated [Bitstream]s into Medusa.
+  # Uploads all of the instance's associated {Bitstream}s into Medusa.
   #
   # @return [void]
   #

app/views/items/_show_button_group.html.haml
@@ -1,6 +1,6 @@
 .btn-group.float-right{role: "group"}
   // Delete button
-  -if !@item.in_archive? && policy(@item).destroy?
+  -if !@item.exists_in_medusa? && policy(@item).destroy?
     = link_to(item_path(@item), class: 'btn btn-danger',
                                     method: :delete,
                                     "data-confirm": "Are you sure you want to delete this item?") do

app/views/items/show.json.jbuilder
@@ -4,7 +4,7 @@ json.set! "class", @item.class.to_s
 json.uri item_url(@item, format: :json)
 
 if policy(@item).show?
-  json.extract! @item, :id, :submitting, :in_archive?, :discoverable, :withdrawn, :created_at, :updated_at
+  json.extract! @item, :id, :submitting, :exists_in_medusa?, :discoverable, :withdrawn, :created_at, :updated_at
   json.primary_collection do
     json.id @item.primary_collection_id
     json.uri collection_url(@item.primary_collection_id, format: :json)

test/controllers/bitstreams_controller_test.rb
@@ -158,26 +158,26 @@ class BitstreamsControllerTest < ActionDispatch::IntegrationTest
   # destroy()
 
   test "destroy() redirects to login page for logged-out users" do
-    delete item_bitstream_path(items(:item1), bitstreams(:item1_jpg))
+    delete item_bitstream_path(items(:item1), bitstreams(:item1_in_staging))
     assert_redirected_to login_path
   end
 
   test "destroy() returns HTTP 403 for unauthorized users" do
     log_in_as(users(:norights))
-    delete item_bitstream_path(items(:item1), bitstreams(:item1_jpg))
+    delete item_bitstream_path(items(:item1), bitstreams(:item1_in_staging))
     assert_response :forbidden
   end
 
   test "destroy() destroys the bitstream" do
     log_in_as(users(:admin))
     assert_difference "Bitstream.count", -1 do
-      delete item_bitstream_path(items(:item1), bitstreams(:item1_jpg))
+      delete item_bitstream_path(items(:item1), bitstreams(:item1_in_staging))
     end
   end
 
   test "destroy() returns HTTP 204 for an existing bitstream" do
     log_in_as(users(:admin))
-    delete item_bitstream_path(items(:item1), bitstreams(:item1_jpg))
+    delete item_bitstream_path(items(:item1), bitstreams(:item1_in_staging))
     assert_response :no_content
   end
 
@@ -191,7 +191,7 @@ class BitstreamsControllerTest < ActionDispatch::IntegrationTest
 
   test "show() returns HTTP 200" do
     item = items(:item1)
-    bs   = bitstreams(:item1_jpg)
+    bs   = bitstreams(:item1_in_staging)
     get item_bitstream_path(item, bs, format: :json)
     assert_response :ok
   end

test/fixtures/bitstreams.yml
@@ -1,6 +1,6 @@
 # Read about fixtures at https://api.rubyonrails.org/classes/ActiveRecord/FixtureSet.html
 
-item1_jpg:
+item1_in_staging:
   exists_in_staging: true
   staging_key: bogus
   length: <%= File.size(File.join(Rails.root, "test", "fixtures", "files", "escher_lego.jpg")) %>
@@ -10,7 +10,7 @@ item1_jpg:
   medusa_uuid:
   item: item1
 
-item1_in_medusa:
+item2_in_medusa:
   exists_in_staging: false
   staging_key: test/test/escher_lego2.jpg
   length: <%= File.size(File.join(Rails.root, "test", "fixtures", "files", "escher_lego.jpg")) %>
@@ -18,4 +18,4 @@ item1_in_medusa:
   original_filename: escher_lego2.jpg
   medusa_key: test/test/escher_lego2.jpg
   medusa_uuid: 3d2a99d5-2f5b-401e-80c1-864a95e3acf7
-  item: item1
+  item: in_medusa

test/fixtures/items.yml
@@ -7,7 +7,7 @@ item1:
   collections:
     - collection1
 
-item2:
+in_medusa:
   submitter: admin
   submitting: false
   discoverable: true

test/models/bitstream_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class BitstreamTest < ActiveSupport::TestCase
 
   setup do
-    @instance = bitstreams(:item1_jpg)
+    @instance = bitstreams(:item1_in_staging)
     assert @instance.valid?
     create_bucket
   end
@@ -70,7 +70,7 @@ class BitstreamTest < ActiveSupport::TestCase
   end
 
   test "delete_from_medusa() sends a correct message if medusa_uuid is set" do
-    @instance = bitstreams(:item1_in_medusa)
+    @instance = bitstreams(:item2_in_medusa)
     @instance.delete_from_medusa
     AmqpHelper::Connector[:ideals].with_parsed_message(MedusaIngest.outgoing_queue) do |message|
       puts message

test/models/incoming_message_test.rb
@@ -7,7 +7,7 @@ class IncomingMessageTest < ActiveSupport::TestCase
   test "handle() handles an ingest-succeeded message" do
     IncomingMessage.destroy_all
     ingest      = medusa_ingests(:one)
-    bitstream   = bitstreams(:item1_jpg)
+    bitstream   = bitstreams(:item1_in_staging)
     medusa_uuid = SecureRandom.uuid
     medusa_message = {
         'status'       => "ok",
@@ -76,7 +76,7 @@ class IncomingMessageTest < ActiveSupport::TestCase
   end
 
   test "handle() deletes any bitstream corresponding to a delete-succeeded message" do
-    bitstream = bitstreams(:item1_in_medusa)
+    bitstream = bitstreams(:item2_in_medusa)
     medusa_message = {
         'status'    => "ok",
         'operation' => "delete",

commit eabe6bde916d233e7f99521b4277f1c9741f8b71

Fix a bug introduced in c27a0b80a799c8546322baea3b527736b4a42cab

app/models/item.rb
@@ -293,7 +293,7 @@ class Item < ApplicationRecord
   private
 
   def restrict_in_archive_deletion
-    raise "Archived items cannot be deleted" if self.in_archive?
+    raise "Archived items cannot be deleted" if self.exists_in_medusa?
   end
 
 end