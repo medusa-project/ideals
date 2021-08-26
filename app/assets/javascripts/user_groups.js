549f03f5d2729a93c88f8a5315fe2b63a9695e31
Alex Dolski <alexd@n-ary.net>
2021-08-26 10:40:39 -0500

    Fix the Add User Group modal

--- app/assets/javascripts/user_groups.js
+++ app/assets/javascripts/user_groups.js
@@ -1,17 +1,33 @@
+const UserGroupForm = {
+    attachEventListeners: function() {
+        $('button.remove').on('click', function () {
+            const row = $(this).closest('.input-group');
+            const siblings = row.siblings('.input-group');
+            if (siblings.length > 0) {
+                row.remove();
+            } else {
+                row.find('input').val('');
+            }
+            return false;
+        });
+        $('button.add').on('click', function () {
+            const lastRow = $(this).closest('form').find('.input-group:last');
+            const clone = lastRow.clone(true);
+            clone.find('input[type=text]').val('');
+            lastRow.after(clone);
+            return false;
+        });
+    }
+}
+
 /**
  * Handles list-user-groups view (/user-groups).
  *
  * @constructor
  */
 const UserGroupsView = function() {
-    const ROOT_URL = $('input[name="root_url"]').val();
-
-    $("button.edit-user-group").on("click", function() {
-        const id = $(this).data("user-group-id");
-        const url = ROOT_URL + "/user-groups/" + id + "/edit";
-        $.get(url, function(data) {
-            $("#edit-user-group-modal .modal-body").html(data);
-        });
+    $("#add-user-group-modal").on("show.bs.modal", function() {
+        UserGroupForm.attachEventListeners();
     });
 };
 
@@ -28,30 +44,9 @@ const UserGroupView = function() {
         const url = ROOT_URL + "/user-groups/" + id + "/edit";
         $.get(url, function(data) {
             $("#edit-user-group-modal .modal-body").html(data);
-            attachFormEventListeners();
+            UserGroupForm.attachEventListeners();
         });
     });
-
-    function attachFormEventListeners() {
-        $('button.remove').on('click', function() {
-            const row = $(this).closest('.input-group');
-            const siblings = row.siblings('.input-group');
-            if (siblings.length > 0) {
-                row.remove();
-            } else {
-                row.find('input').val('');
-            }
-            return false;
-        });
-        $('button.add').on('click', function() {
-            const lastRow = $(this).closest('form').find('.input-group:last');
-            const clone = lastRow.clone(true);
-            clone.find('input[type=text]').val('');
-            lastRow.after(clone);
-            return false;
-        });
-    }
-
 };
 
 $(document).ready(function() {
