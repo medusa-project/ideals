c2977b207d6db174dce1c26d9dbbd97366b1c72d
Alex Dolski <alexd@n-ary.net>
2021-09-01 15:39:27 -0500

    Initial commit

---
+++ test/models/file_format_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class FileFormatTest < ActiveSupport::TestCase
+
+  test "for_extension() returns a format for the given extension" do
+    format = FileFormat.for_extension("jpg")
+    assert_equal "image", format.category
+    assert_equal %w(jpg jpeg), format.extensions
+    assert_equal "file-image-o", format.icon
+    assert_equal "JPEG JFIF", format.long_name
+    assert_equal %w(image/jpeg), format.media_types
+    assert_equal "JPEG", format.short_name
+    assert format.readable_by_vips
+  end
+
+  test "for_extension() returns nil for an unrecognized extension" do
+    assert_nil FileFormat.for_extension("bogus")
+  end
+
+end
