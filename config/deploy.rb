b4c3c6f0551a6a9b82a47bda4b4fb5a95a9db3e3
Alex Dolski <alexd@n-ary.net>
2021-09-29 10:05:35 -0500

    Remove nginx.conf.erb

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,8 +25,7 @@ set :passenger_restart_with_touch, true
 set :linked_files, fetch(:linked_files, []).push(
     'config/credentials/demo.key',
     'config/credentials/production.key',
-    'public/robots.txt',
-    'nginx.conf.erb')
+    'public/robots.txt')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log',
