d1dd3a9ccef217d96d6d7928bf0072f4b10414ac
Alex Dolski <alexd@n-ary.net>
2022-01-11 09:39:17 -0600

    The sleeping is not necessary

--- app/jobs/import_saf_package_job.rb
+++ app/jobs/import_saf_package_job.rb
@@ -9,9 +9,6 @@ class ImportSafPackageJob < ApplicationJob
   # @raises [ArgumentError]
   #
   def perform(*args)
-    # Files to import have just been uploaded to S3. S3 is only eventually
-    # consistent, so we want to give it time for all of them to show up.
-    sleep 2
     SafImporter.new.import_from_s3(args[0])
   end
 
