cf6aa340127465597ffcbadc80847aeff1700509
Alex Dolski <alexd@n-ary.net>
2022-04-25 10:07:03 -0500

    Set config.active_job.queue_adapter = :test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,8 +27,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
-  config.active_job.queue_adapter = :async
-  config.active_job.queue_name_prefix = "ideals_test"
+  config.active_job.queue_adapter = :test
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
