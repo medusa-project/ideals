0a4afe0755fd7aaea80b1e609b916c81b6acc0e0
Alex Dolski <alexd@n-ary.net>
2022-05-02 14:32:46 -0500

    Add a before_changes argument to the initializer

--- app/commands/update_item_command.rb
+++ app/commands/update_item_command.rb
@@ -1,15 +1,23 @@
 ##
-# Contrary to its name, this class doesn't actually directly update an {Item}.
+# Contrary to its name, this class doesn't actually directly update an [Item].
 # Instead, {execute} executes a block which contains item-updating code.
 # This enables complex and multi-step updates while still building a correct
-# {Event}.
+# [Event].
 #
 class UpdateItemCommand < Command
 
-  def initialize(item:, user:, description: nil)
-    @item        = item
-    @user        = user
-    @description = description
+  ##
+  # @param item [Item]
+  # @param user [User]
+  # @param description [String]
+  # @param before_changes [Hash] If not provided, the item's current
+  #                              {as_change_hash change hash} is used.
+  #
+  def initialize(item:, user:, description: nil, before_changes: nil)
+    @item           = item
+    @user           = user
+    @description    = description
+    @before_changes = before_changes
   end
 
   ##
@@ -17,12 +25,11 @@ class UpdateItemCommand < Command
   #
   def execute(&block)
     Item.transaction do
-      before_changes = @item.as_change_hash
       yield(@item)
       Event.create!(event_type:     Event::Type::UPDATE,
                     item:           @item,
                     user:           @user,
-                    before_changes: before_changes,
+                    before_changes: @before_changes || @item.as_change_hash,
                     after_changes:  @item.as_change_hash,
                     description:    @description)
     end
