medusa-project/ideals

commit d0fc520b1585207f89d7c11f511c8b7cb6f64875

Rename metadata_profile_elements.index & submission_profile_elements.index to position

app/models/submission_profile_element.rb
@@ -1,21 +1,21 @@
 ##
-# Encapsulates an element in a {SubmissionProfile}. Essentially this is a
-# glorified join model between {SubmissionProfile} and {RegisteredElement}.
+# Encapsulates an element in a [SubmissionProfile]. Essentially this is a
+# glorified join model between [SubmissionProfile] and [RegisteredElement].
 #
 # # Attributes
 #
 # * `created_at`            Managed by ActiveRecord.
 # * `help_text`             Help text.
-# * `index`                 Zero-based position within the owning
-#                           {SubmissionProfile}.
 # * `placeholder_text`      Text that is inserted into form inputs by default.
-# * `registered_element_id` ID of the associated {RegisteredElement}. Foreign
+# * `position`              Zero-based position within the owning
+#                           [SubmissionProfile].
+# * `registered_element_id` ID of the associated [RegisteredElement]. Foreign
 #                           key.
-# * `repeatable`            Whether multiple matching {AscribedElement}s can be
+# * `repeatable`            Whether multiple matching [AscribedElement]s can be
 #                           associated with an entity.
-# * `required`              Whether a matching {AscribedElement} must be
+# * `required`              Whether a matching [AscribedElement] must be
 #                           associated with an entity.
-# * `submission_profile_id` ID of the associated {SubmissionProfile}. Foreign
+# * `submission_profile_id` ID of the associated [SubmissionProfile]. Foreign
 #                           key.
 # * `updated_at`            Managed by ActiveRecord.
 #
@@ -24,19 +24,19 @@ class SubmissionProfileElement < ApplicationRecord
   belongs_to :submission_profile, inverse_of: :elements, touch: true
   belongs_to :registered_element, inverse_of: :submission_profile_elements
 
-  # index -- note that uniqueness is not validated here as we are going to
+  # Note that uniqueness is not validated here as we are going to
   # handle that in persistence callbacks instead.
-  validates :index, numericality: { only_integer: true,
-                                    greater_than_or_equal_to: 0 }
-  validates_presence_of :index
+  validates :position, numericality: { only_integer: true,
+                                       greater_than_or_equal_to: 0 }
+  validates_presence_of :position
 
   # registered_element_id
   validates_presence_of :registered_element_id
   validates_uniqueness_of :registered_element_id, scope: :submission_profile_id
 
-  before_create :shift_element_indexes_before_create
-  before_update :shift_element_indexes_before_update
-  after_destroy :shift_element_indexes_after_destroy
+  before_create :shift_element_positions_before_create
+  before_update :shift_element_positions_before_update
+  after_destroy :shift_element_positions_after_destroy
 
   ##
   # @return [String] The `label` property of the associated
@@ -57,39 +57,39 @@ class SubmissionProfileElement < ApplicationRecord
   private
 
   ##
-  # Increments the indexes of all elements in the owning {SubmissionProfile}
-  # that are greater than or equal to the index of this instance, in order to
-  # make room for it.
+  # Increments the positions of all elements in the owning [SubmissionProfile]
+  # that are greater than or equal to the position of this instance, in order
+  # to make room for it.
   #
-  def shift_element_indexes_before_create
+  def shift_element_positions_before_create
     transaction do
-      self.submission_profile.elements.where("index >= ?", self.index).each do |e|
+      self.submission_profile.elements.where("position >= ?", self.position).each do |e|
         # update_column skips callbacks, which would cause this method to
         # be called recursively.
-        e.update_column(:index, e.index + 1)
+        e.update_column(:position, e.position + 1)
       end
     end
   end
 
   ##
-  # Updates the indexes of all elements in the owning {SubmissionProfile} to
+  # Updates the positions of all elements in the owning [SubmissionProfile] to
   # ensure that they are sequential.
   #
-  def shift_element_indexes_before_update
-    if self.index_changed? && self.submission_profile
-      min       = [self.index_was, self.index].min
-      max       = [self.index_was, self.index].max
-      increased = (self.index_was < self.index)
+  def shift_element_positions_before_update
+    if self.position_changed? && self.submission_profile
+      min       = [self.position_was, self.position].min
+      max       = [self.position_was, self.position].max
+      increased = (self.position_was < self.position)
 
       transaction do
         self.submission_profile.elements.
-            where('id != ? AND index >= ? AND index <= ?', self.id, min, max).each do |e|
+            where('id != ? AND position >= ? AND position <= ?', self.id, min, max).each do |e|
           if increased # shift the range down
             # update_column skips callbacks, which would cause this method to
             # be called recursively.
-            e.update_column(:index, e.index - 1)
+            e.update_column(:position, e.position - 1)
           else # shift it up
-            e.update_column(:index, e.index + 1)
+            e.update_column(:position, e.position + 1)
           end
         end
       end
@@ -97,16 +97,16 @@ class SubmissionProfileElement < ApplicationRecord
   end
 
   ##
-  # Updates the indexes of all elements in the owning {SubmissionProfile} to
+  # Updates the positions of all elements in the owning [SubmissionProfile] to
   # ensure that they are sequential and zero-based.
   #
-  def shift_element_indexes_after_destroy
+  def shift_element_positions_after_destroy
     if self.submission_profile && self.destroyed?
       transaction do
-        self.submission_profile.elements.order(:index).each_with_index do |element, index|
+        self.submission_profile.elements.order(:position).each_with_index do |element, index|
           # update_column skips callbacks, which would cause this method to be
           # called recursively.
-          element.update_column(:index, index) if element.index != index
+          element.update_column(:position, position) if element.position != position
         end
       end
     end

app/controllers/metadata_profile_elements_controller.rb
@@ -67,9 +67,9 @@ class MetadataProfileElementsController < ApplicationController
   private
 
   def element_params
-    params.require(:metadata_profile_element).permit(:faceted, :index,
-                                                     :indexed,
+    params.require(:metadata_profile_element).permit(:faceted, :indexed,
                                                      :metadata_profile_id,
+                                                     :position,
                                                      :registered_element_id,
                                                      :searchable, :sortable,
                                                      :visible)

app/controllers/submission_profile_elements_controller.rb
@@ -68,7 +68,7 @@ class SubmissionProfileElementsController < ApplicationController
   private
 
   def element_params
-    params.require(:submission_profile_element).permit(:help_text, :index,
+    params.require(:submission_profile_element).permit(:help_text, :position,
                                                        :placeholder_text,
                                                        :registered_element_id,
                                                        :repeatable, :required,

app/helpers/application_helper.rb
@@ -210,7 +210,7 @@ module ApplicationHelper
     # Find all registered elements that have Highwire mappings.
     reg_elements = entity.effective_metadata_profile.elements.
       where(visible: true).
-      order(:index).
+      order(:position).
       map(&:registered_element).
       select{ |e| e.highwire_mapping.present? }
     name_value_map = {} # hash of Highwire name keys -> sanitized values
@@ -298,7 +298,7 @@ module ApplicationHelper
     html = StringIO.new
     html << "<dl class=\"metadata\">"
     reg_elements = profile ?
-                       profile.elements.where(visible: true).order(:index) :
+                       profile.elements.where(visible: true).order(:position) :
                        RegisteredElement.all.order(:label)
     reg_elements.each do |element|
       matching_ascribed_elements = ascribed_elements.
@@ -335,7 +335,7 @@ module ApplicationHelper
   def metadata_as_meta_tags(ascribed_elements, profile = nil)
     html = StringIO.new
     reg_elements = profile ?
-                     profile.elements.where(visible: true).order(:index) :
+                     profile.elements.where(visible: true).order(:position) :
                      RegisteredElement.all.order(:label)
     reg_elements.each do |reg_element|
       ascribed_elements.

app/helpers/items_helper.rb
@@ -6,7 +6,7 @@ module ItemsHelper
   #                  outer `form` element.
   #
   def advanced_search_form(metadata_profile = MetadataProfile.default)
-    mp_elements = metadata_profile.elements.select(&:searchable).sort_by(&:index)
+    mp_elements = metadata_profile.elements.select(&:searchable).sort_by(&:position)
     html     = StringIO.new
 
     mp_elements.each do |mp_e|

app/models/metadata_profile.rb
@@ -34,7 +34,7 @@ class MetadataProfile < ApplicationRecord
 
   has_many :collections, inverse_of: :metadata_profile,
            dependent: :restrict_with_exception
-  has_many :elements, -> { order(:index) },
+  has_many :elements, -> { order(:position) },
            class_name: "MetadataProfileElement", inverse_of: :metadata_profile,
            dependent: :destroy
 
@@ -57,56 +57,56 @@ class MetadataProfile < ApplicationRecord
   def add_default_elements
     raise "Instance already has elements ascribed to it" if self.elements.any?
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:title"),
-                        index:              0,
+                        position:           0,
                         visible:            true,
                         searchable:         true,
                         sortable:           true,
                         faceted:            false,
                         indexed:            true)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:subject"),
-                        index:              1,
+                        position:           1,
                         visible:            true,
                         searchable:         true,
                         sortable:           true,
                         faceted:            true,
                         indexed:            true)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:creator"),
-                        index:              2,
+                        position:           2,
                         visible:            true,
                         searchable:         true,
                         sortable:           true,
                         faceted:            true,
                         indexed:            true)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:contributor"),
-                        index:              3,
+                        position:           3,
                         visible:            true,
                         searchable:         true,
                         sortable:           false,
                         faceted:            false,
                         indexed:            true)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:description:abstract"),
-                        index:              4,
+                        position:           4,
                         visible:            true,
                         searchable:         true,
                         sortable:           false,
                         faceted:            false,
                         indexed:            true)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:date:issued"),
-                        index:              5,
+                        position:           5,
                         visible:            true,
                         searchable:         true,
                         sortable:           true,
                         faceted:            false,
                         indexed:            true)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:identifier:uri"),
-                        index:              6,
+                        position:           6,
                         visible:            true,
                         searchable:         true,
                         sortable:           false,
                         faceted:            false,
                         indexed:            true)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:type"),
-                        index:              7,
+                        position:           7,
                         visible:            true,
                         searchable:         true,
                         sortable:           false,
@@ -139,7 +139,7 @@ class MetadataProfile < ApplicationRecord
   # @return [Enumerable<MetadataProfileElement>]
   #
   def faceted_elements
-    self.elements.where(faceted: true).order(:index)
+    self.elements.where(faceted: true).order(:position)
   end
 
 

app/models/metadata_profile_element.rb
@@ -1,18 +1,18 @@
 ##
-# Encapsulates an element in a {MetadataProfile}. Essentially this is a
-# glorified join model between {MetadataProfile} and {RegisteredElement}.
+# Encapsulates an element in a [MetadataProfile]. Essentially this is a
+# glorified join model between [MetadataProfile] and [RegisteredElement].
 #
 # # Attributes
 #
 # * `created_at`            Managed by ActiveRecord.
 # * `faceted`               Whether the element is used to provide facets in
 #                           results views.
-# * `index`                 Zero-based position within the owning
-#                           {MetadataProfile}.
 # * `indexed`               Whether the element is indexed in searchable
 #                           documents.
 # * `metadata_profile_id`   ID of the associated metadata profile. Foreign key.
-# * `registered_element_id` ID of the associated {RegisteredElement}. Foreign
+# * `position`              Zero-based position relative to other elements in
+#                           the owning [MetadataProfile].
+# * `registered_element_id` ID of the associated [RegisteredElement]. Foreign
 #                           key.
 # * `searchable`            Whether users can search on the element.
 # * `sortable`              Whether results can be sorted on the element.
@@ -23,19 +23,19 @@ class MetadataProfileElement < ApplicationRecord
   belongs_to :metadata_profile, inverse_of: :elements, touch: true
   belongs_to :registered_element, inverse_of: :metadata_profile_elements
 
-  # index -- note that uniqueness is not validated here as we are going to
-  # handle that in persistence callbacks instead.
-  validates :index, numericality: { only_integer: true,
-                                    greater_than_or_equal_to: 0 }
-  validates_presence_of :index
+  # Note that uniqueness is not validated here as we are going to handle that
+  # in persistence callbacks instead.
+  validates :position, numericality: { only_integer: true,
+                                       greater_than_or_equal_to: 0 }
+  validates_presence_of :position
 
   # registered_element_id
   validates_presence_of :registered_element_id
   validates_uniqueness_of :registered_element_id, scope: :metadata_profile_id
 
-  before_create :shift_element_indexes_before_create
-  before_update :shift_element_indexes_before_update
-  after_destroy :shift_element_indexes_after_destroy
+  before_create :shift_element_positions_before_create
+  before_update :shift_element_positions_before_update
+  after_destroy :shift_element_positions_after_destroy
 
   ##
   # @return [String] Label of the associated {RegisteredElement}.
@@ -54,39 +54,39 @@ class MetadataProfileElement < ApplicationRecord
   private
 
   ##
-  # Increments the indexes of all elements in the owning {MetadataProfile} that
-  # are greater than or equal to the index of this instance, in order to make
-  # room for it.
+  # Increments the positions of all elements in the owning [MetadataProfile]
+  # that are greater than or equal to the position of this instance, in order
+  # to make room for it.
   #
-  def shift_element_indexes_before_create
+  def shift_element_positions_before_create
     transaction do
-      self.metadata_profile.elements.where("index >= ?", self.index).each do |e|
-        # update_column skips callbacks, which would cause this method to
-        # be called recursively.
-        e.update_column(:index, e.index + 1)
+      self.metadata_profile.elements.where("position >= ?", self.position).each do |e|
+        # update_column skips callbacks, which would cause this method to be
+        # called recursively.
+        e.update_column(:position, e.position + 1)
       end
     end
   end
 
   ##
-  # Updates the indexes of all elements in the owning {MetadataProfile} to
+  # Updates the positions of all elements in the owning [MetadataProfile] to
   # ensure that they are sequential.
   #
-  def shift_element_indexes_before_update
-    if self.index_changed? && self.metadata_profile
-      min       = [self.index_was, self.index].min
-      max       = [self.index_was, self.index].max
-      increased = (self.index_was < self.index)
+  def shift_element_positions_before_update
+    if self.position_changed? && self.metadata_profile
+      min       = [self.position_was, self.position].min
+      max       = [self.position_was, self.position].max
+      increased = (self.position_was < self.position)
 
       transaction do
         self.metadata_profile.elements.
-            where('id != ? AND index >= ? AND index <= ?', self.id, min, max).each do |e|
+            where('id != ? AND position >= ? AND position <= ?', self.id, min, max).each do |e|
           if increased # shift the range down
             # update_column skips callbacks, which would cause this method to
             # be called recursively.
-            e.update_column(:index, e.index - 1)
+            e.update_column(:position, e.position - 1)
           else # shift it up
-            e.update_column(:index, e.index + 1)
+            e.update_column(:position, e.position + 1)
           end
         end
       end
@@ -94,16 +94,16 @@ class MetadataProfileElement < ApplicationRecord
   end
 
   ##
-  # Updates the indexes of all elements in the owning {MetadataProfile} to
+  # Updates the positions of all elements in the owning [MetadataProfile] to
   # ensure that they are sequential and zero-based.
   #
-  def shift_element_indexes_after_destroy
+  def shift_element_positions_after_destroy
     if self.metadata_profile && self.destroyed?
       transaction do
-        self.metadata_profile.elements.order(:index).each_with_index do |element, index|
+        self.metadata_profile.elements.order(:position).each_with_index do |element, position|
           # update_column skips callbacks, which would cause this method to be
           # called recursively.
-          element.update_column(:index, index) if element.index != index
+          element.update_column(:position, position) if element.position != position
         end
       end
     end

app/models/submission_profile.rb
@@ -32,7 +32,7 @@ class SubmissionProfile < ApplicationRecord
 
   has_many :collections, inverse_of: :submission_profile,
            dependent: :restrict_with_exception
-  has_many :elements, -> { order(:index) },
+  has_many :elements, -> { order(:position) },
            class_name: "SubmissionProfileElement",
            inverse_of: :submission_profile,
            dependent: :destroy
@@ -55,99 +55,99 @@ class SubmissionProfile < ApplicationRecord
   def add_default_elements
     raise "Instance already has elements ascribed to it" if self.elements.any?
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:title"),
-                        index:              0,
+                        position:           0,
                         repeatable:         false,
                         required:           true)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:date:issued"),
-                        index:              1,
+                        position:           1,
                         repeatable:         true,
                         required:           true)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:type"),
-                        index:              2,
+                        position:           2,
                         repeatable:         true,
                         required:           true)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:subject"),
-                        index:              3,
+                        position:           3,
                         repeatable:         true,
                         required:           true)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:creator"),
-                        index:              4,
+                        position:           4,
                         repeatable:         true,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:contributor"),
-                        index:              5,
+                        position:           5,
                         repeatable:         true,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:description:abstract"),
-                        index:              6,
+                        position:           6,
                         repeatable:         false,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:coverage:spatial"),
-                        index:              7,
+                        position:           7,
                         repeatable:         true,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:type:genre"),
-                        index:              8,
+                        position:           8,
                         repeatable:         true,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:language"),
-                        index:              9,
+                        position:           9,
                         repeatable:         false,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:identifier:bibliographicCitation"),
-                        index:              10,
+                        position:           10,
                         repeatable:         false,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:publisher"),
-                        index:              11,
+                        position:           11,
                         repeatable:         false,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:relation:ispartof"),
-                        index:              12,
+                        position:           12,
                         repeatable:         true,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:description:sponsorship"),
-                        index:              13,
+                        position:           13,
                         repeatable:         true,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:rights"),
-                        index:              14,
+                        position:           14,
                         repeatable:         true,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:identifier"),
-                        index:              15,
+                        position:           15,
                         repeatable:         true,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("thesis:degree:name"),
-                        index:              16,
+                        position:           16,
                         repeatable:         false,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("thesis:degree:level"),
-                        index:              17,
+                        position:           17,
                         repeatable:         false,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:contributor:committeeChair"),
-                        index:              18,
+                        position:           18,
                         repeatable:         true,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("dc:contributor:advisor"),
-                        index:              19,
+                        position:           19,
                         repeatable:         true,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("thesis:degree:grantor"),
-                        index:              20,
+                        position:           20,
                         repeatable:         false,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("thesis:degree:discipline"),
-                        index:              21,
+                        position:           21,
                         repeatable:         false,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("thesis:degree:department"),
-                        index:              22,
+                        position:           22,
                         repeatable:         false,
                         required:           false)
     self.elements.build(registered_element: RegisteredElement.find_by_name("thesis:degree:program"),
-                        index:              23,
+                        position:           23,
                         repeatable:         false,
                         required:           false)
     self.save!

commit c77296a4fe78c5bc72e51dd71f06380d4613e8bb

Fix a bug introduced in d0fc520b1585207f89d7c11f511c8b7cb6f64875

app/models/submission_profile_element.rb
@@ -103,7 +103,7 @@ class SubmissionProfileElement < ApplicationRecord
   def shift_element_positions_after_destroy
     if self.submission_profile && self.destroyed?
       transaction do
-        self.submission_profile.elements.order(:position).each_with_index do |element, index|
+        self.submission_profile.elements.order(:position).each_with_index do |element, position|
           # update_column skips callbacks, which would cause this method to be
           # called recursively.
           element.update_column(:position, position) if element.position != position