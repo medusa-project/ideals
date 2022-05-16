medusa-project/ideals

commit 467823464b1b6c4599c3614ca542878d60a00653

Fix initial collection selection when submitting from a collection page

app/controllers/submissions_controller.rb
@@ -58,14 +58,15 @@ class SubmissionsController < ApplicationController
   end
 
   ##
-  # Creates a new {Item} upon acceptance of the {new deposit agreement}.
+  # Creates a new [Item] upon acceptance of the {new deposit agreement}.
   # After the submission has been created, the user is redirected to {edit}.
   #
   # Responds to `POST /submissions`.
   #
   def create
-    command = CreateItemCommand.new(submitter: current_user)
-    item    = command.execute
+    collection = Collection.find_by(id: params[:primary_collection_id]) # may be nil
+    item       = CreateItemCommand.new(submitter:          current_user,
+                                       primary_collection: collection).execute
     authorize item, policy_class: SubmissionPolicy # this should always succeed
     redirect_to edit_submission_path(item)
   end
@@ -110,8 +111,11 @@ class SubmissionsController < ApplicationController
     # This will be nil if we are arriving at /submit, but otherwise we will
     # use it to pre-select the collection in the submission form.
     @collection  = Collection.find_by(id: params[:collection_id])
+    # These are all submissions in the same collection, or no collection.
     @submissions = current_user.submitted_items.
+      joins(:collection_item_memberships).
       where(stage: Item::Stages::SUBMITTING).
+      where("collection_item_memberships.collection_id": @collection&.id).
       order(:updated_at)
   end
 

app/assets/javascripts/submissions.js
@@ -4,10 +4,6 @@
  * @constructor
  */
 const AgreementView = function() {
-    if ($("#submissions-in-progress").length === 0) {
-        $("#questions").show();
-    }
-
     // Switch the plus icon in the expand-deposit-agreement section to a minus
     // icon upon click, and vice versa.
     const depositAgreementSection = $('#deposit-agreement');

app/assets/stylesheets/submissions.scss
@@ -11,10 +11,6 @@ body#deposit-agreement-body {
       text-decoration: none;
     }
   }
-
-  #questions {
-    display: none;
-  }
 }
 
 body#edit_submission {

app/views/submissions/new.html.haml
@@ -1,7 +1,12 @@
 - provide :body_id, "deposit-agreement-body"
 - provide :title, "Submit an Item"
 
-%h1 Submit an Item
+%h1
+  Submit an Item
+  - if @collection
+    %small
+      to
+      = @collection.title
 
 - if @submissions.any?
   #submissions-in-progress.card.text-white.bg-info.mb-3
@@ -16,9 +21,9 @@
             - else
               = link_to(raw("Started #{local_time_ago(item.created_at)} (collection not selected)"),
                         edit_submission_path(item))
-  %button.begin-submission.btn.btn-outline-primary Begin New Submission
 
-#deposit-agreement.accordion.mb-3{style: "#{@submissions.any? ? "display: none" : ""}"}
+
+#deposit-agreement.accordion.mb-3
   %p To start your deposit, please review the deposit agreement and answer the following questions.
 
   .card
@@ -120,7 +125,7 @@
   %hr
 
   = form_tag(submissions_path, method: :post) do
-    = hidden_field_tag(:primary_collection_id, params[:collection_id])
+    = hidden_field_tag(:primary_collection_id, @collection&.id)
     .row
       .col-sm-3
         = submit_tag("Begin Submission", class: "btn btn-secondary btn-lg")

commit 35c644bc983ec9eeed97e9462b6979d0fe71275a

Fix a bug introduced in 467823464b1b6c4599c3614ca542878d60a00653 (IR-230)

app/controllers/submissions_controller.rb
@@ -115,8 +115,11 @@ class SubmissionsController < ApplicationController
     @submissions = current_user.submitted_items.
       joins(:collection_item_memberships).
       where(stage: Item::Stages::SUBMITTING).
-      where("collection_item_memberships.collection_id": @collection&.id).
       order(:updated_at)
+    if @collection
+      @submissions = @submissions.
+        where("collection_item_memberships.collection_id": @collection.id)
+    end
   end
 
   ##