medusa-project/ideals

commit b55a6ff61b81e1f805a7b8675c0154a6bfcf1f5c

Logic fix in User.effective_manager?() (IR-236)

app/controllers/collections_controller.rb
@@ -37,11 +37,11 @@ class CollectionsController < ApplicationController
   # Responds to `POST /collections`.
   #
   def create
-    @collection = Collection.new(collection_params)
+    @collection              = Collection.new(collection_params)
+    @collection.primary_unit = Unit.find(params[:primary_unit_id])
     authorize @collection
     begin
       ActiveRecord::Base.transaction do
-        @collection.primary_unit = Unit.find(params[:primary_unit_id])
         # Save now in order to obtain an ID with which to associate
         # AscribedElements in the next step.
         @collection.save!

app/models/user.rb
@@ -110,7 +110,7 @@ class User < ApplicationRecord
     # Check for sysadmin.
     return true if sysadmin?
     # Check for unit admin.
-    collection.units.each do |unit|
+    (collection.all_units + [collection.primary_unit]).each do |unit|
       return true if effective_unit_admin?(unit)
     end
     # Check for manager of the collection itself.

commit cfdace384b27b84ebcc794531725a03fcefa0612

Fix a bug introduced in b55a6ff61b81e1f805a7b8675c0154a6bfcf1f5c

app/controllers/collections_controller.rb
@@ -38,7 +38,7 @@ class CollectionsController < ApplicationController
   #
   def create
     @collection              = Collection.new(collection_params)
-    @collection.primary_unit = Unit.find(params[:primary_unit_id])
+    @collection.primary_unit = Unit.find_by_id(params[:primary_unit_id])
     authorize @collection
     begin
       ActiveRecord::Base.transaction do