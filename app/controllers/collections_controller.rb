medusa-project/ideals

commit 87de4268484ca3e92880b85f4a35c344bf65c75f

Fix inability of non-sysadmins to create collections (IR-236)

app/controllers/collections_controller.rb
@@ -38,15 +38,15 @@ class CollectionsController < ApplicationController
   # Responds to `POST /collections`.
   #
   def create
-    @collection              = Collection.new(collection_params)
-    @collection.primary_unit = Unit.find_by_id(params[:primary_unit_id])
-    authorize @collection
     begin
       ActiveRecord::Base.transaction do
-        # Save now in order to obtain an ID with which to associate
-        # AscribedElements in the next step.
+        @collection              = Collection.new(collection_params)
+        @collection.primary_unit = Unit.find_by_id(params[:primary_unit_id])
+        # We need to save now in order to assign the collection an ID which
+        # many of the authorization methods will need. If authorization fails,
+        # the transaction will roll back.
         @collection.save!
-        assign_users
+        authorize @collection
         @collection.save!
       end
     rescue => e

commit d35142d4f14a05d5ffcf06d61128854c82828450

Fix a regression introduced in 87de4268484ca3e92880b85f4a35c344bf65c75f

app/controllers/collections_controller.rb
@@ -49,6 +49,8 @@ class CollectionsController < ApplicationController
         authorize @collection
         @collection.save!
       end
+    rescue NotAuthorizedError => e
+      raise e
     rescue => e
       render partial: "shared/validation_messages",
              locals: { object: @collection.errors.any? ? @collection : e },