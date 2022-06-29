7ddcb6822dc21e8feb7e44b5757e282691c5ffed
Alex Dolski <alexd@n-ary.net>
2022-06-29 15:15:26 -0500

    from_omniauth() falls back to looking up by email after uid

--- app/models/shibboleth_user.rb
+++ app/models/shibboleth_user.rb
@@ -13,7 +13,8 @@ class ShibbolethUser < User
   #
   def self.from_omniauth(auth)
     auth = auth.deep_stringify_keys
-    user = ShibbolethUser.find_by(uid: auth["uid"])
+    user = ShibbolethUser.find_by(uid: auth["uid"]) ||
+           ShibbolethUser.find_by(email: auth.dig("info", "email"))
     if user
       user.update_with_omniauth(auth)
     else
