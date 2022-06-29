medusa-project/ideals

commit 2950066a52890b4d28096579cba94d7d1d8268db

Load the file navigator asynchronously (IR-247)

app/assets/javascripts/items.js
@@ -54,108 +54,127 @@ const ItemsView = function() {
 /**
  * Controls the file navigator in item view.
  *
- * Events:
- *
- * * `IDEALS.FileNavigator.fileChanged`
- *
  * @constructor
  */
 const FileNavigator = function() {
     const HEADER_HEIGHT = 50;
     const FOOTER_HEIGHT = 50;
-    const ROOT_URL      = $('input[name="root_url"]').val();
+    const ROOT_URL      = $('input[name=root_url]').val();
+    const ITEM_ID       = $("input[name=item_id]").val();
     const navigator     = $("#file-navigator");
-    const thumbsColumn  = $("#file-navigator-thumbnail-column");
-    const viewerColumn  = $("#file-navigator-viewer-column");
 
-    var currentBitstreamID;
+    // Set the initial height of the viewer container.
+    const navigatorHeight = window.innerHeight - 100;
+    navigator.css("height", navigatorHeight + "px");
 
-    const attachEventListeners = function() {
-        $("button#more-info").on("click", function() {
-            const infoDiv = $("#file-navigator-viewer-info");
-            if (infoDiv.is(":visible")) {
-                $(this).html("<i class=\"fa fa-info-circle\"></i> More Info");
-                infoDiv.hide();
-            } else {
-                $(this).html("<i class=\"fa fa-times\"></i> Hide Info");
-                infoDiv.css("display", "flex");
-            }
-        });
-    };
-
-    const focus = function(thumb) {
-        const item_id      = thumb.data("item-id");
-        const bitstream_id = thumb.data("bitstream-id");
-        if (bitstream_id === currentBitstreamID) {
-            return;
+    // Load the navigator HTML.
+    $.ajax({
+        method: "GET",
+        url:    ROOT_URL + "/items/" + ITEM_ID + "/file-navigator",
+        success: function(data) {
+            navigator.css("display", "grid");
+            navigator.css("grid-template-columns", "30% 70%");
+            navigator.html(data);
+            navigator.trigger("IDEALS.FileNavigator.thumbsLoaded");
+        },
+        error: function(data, status, xhr) {
         }
-        currentBitstreamID = bitstream_id;
-        viewerColumn.html(IDEALS.Spinner());
-        thumb.siblings().removeClass("selected");
-        thumb.addClass("selected");
-        thumb.focus();
+    });
 
-        const url = ROOT_URL + "/items/" + item_id + "/bitstreams/" +
-            bitstream_id + "/viewer";
-        $.ajax({
-            method: "GET",
-            url: url,
-            success: function(data) {
-                viewerColumn.html(data);
-                updateHeight();
-                attachEventListeners();
-                navigator.trigger("IDEALS.FileNavigator.fileChanged");
-            },
-            error: function(data, status, xhr) {
-                $("#file-navigator-viewer-column .spinner-border").hide();
-                viewerColumn.html("<div id='file-navigator-viewer-error'><p>There was an error retrieving this file.</p></div>");
-            }
-        });
-    };
+    navigator.on("IDEALS.FileNavigator.thumbsLoaded", function() {
+        const thumbsColumn  = $("#file-navigator-thumbnail-column");
+        const viewerColumn  = $("#file-navigator-viewer-column");
 
-    const updateHeight = function() {
-        const navigatorHeight = window.innerHeight - 100;
-        navigator.css("height", navigatorHeight + "px");
-        const isFooterExisting = thumbsColumn.find("#file-navigator-thumbnail-column-footer").length > 0;
-        const footerHeight     = FOOTER_HEIGHT - (isFooterExisting ? 0 : FOOTER_HEIGHT);
-        thumbsColumn.find("#file-navigator-thumbnail-content").css("height", (navigatorHeight - footerHeight) + "px");
-        thumbsColumn.find("#file-navigator-thumbnail-column-footer").css("height", footerHeight + "px");
-        viewerColumn.find("#file-navigator-viewer-header").css("height", HEADER_HEIGHT + "px");
-        viewerColumn.find("#file-navigator-viewer-content").css("height", (navigatorHeight - HEADER_HEIGHT) + "px");
-        viewerColumn.find("#file-navigator-viewer-content img").css("max-height", (navigatorHeight - HEADER_HEIGHT) + "px");
-    };
+        const updateHeight = function() {
+            const navigatorHeight = window.innerHeight - 100;
+            navigator.css("height", navigatorHeight + "px");
+            const isFooterExisting = thumbsColumn.find("#file-navigator-thumbnail-column-footer").length > 0;
+            const footerHeight     = FOOTER_HEIGHT - (isFooterExisting ? 0 : FOOTER_HEIGHT);
+            thumbsColumn.find("#file-navigator-thumbnail-content").css("height", (navigatorHeight - footerHeight) + "px");
+            thumbsColumn.find("#file-navigator-thumbnail-column-footer").css("height", footerHeight + "px");
+            viewerColumn.find("#file-navigator-viewer-header").css("height", HEADER_HEIGHT + "px");
+            viewerColumn.find("#file-navigator-viewer-content").css("height", (navigatorHeight - HEADER_HEIGHT) + "px");
+            viewerColumn.find("#file-navigator-viewer-content img").css("max-height", (navigatorHeight - HEADER_HEIGHT) + "px");
+        };
 
-    updateHeight();
-
-    $(window).on("resize", function() {
         updateHeight();
-    });
 
-    navigator.find("#file-navigator-thumbnail-column .thumbnail").on("keydown", function(e) {
-        const thumb = $(this);
-        switch (e.keyCode) {
-            case 37: // left arrow
-                focus(thumb.prev());
-                break;
-            case 39: // right arrow
-                focus(thumb.next());
-                break;
+        $(window).on("resize", function() {
+            updateHeight();
+        });
+
+        var currentBitstreamID;
+        const attachEventListeners = function() {
+            $("button#more-info").on("click", function() {
+                const infoDiv = $("#file-navigator-viewer-info");
+                if (infoDiv.is(":visible")) {
+                    $(this).html("<i class=\"fa fa-info-circle\"></i> More Info");
+                    infoDiv.hide();
+                } else {
+                    $(this).html("<i class=\"fa fa-times\"></i> Hide Info");
+                    infoDiv.css("display", "flex");
+                }
+            });
+        };
+
+        const focus = function(thumb) {
+            const item_id      = thumb.data("item-id");
+            const bitstream_id = thumb.data("bitstream-id");
+            if (bitstream_id === currentBitstreamID) {
+                return;
+            }
+            currentBitstreamID = bitstream_id;
+            viewerColumn.html(IDEALS.Spinner());
+            thumb.siblings().removeClass("selected");
+            thumb.addClass("selected");
+            thumb.focus();
+
+            const url = ROOT_URL + "/items/" + item_id + "/bitstreams/" +
+                bitstream_id + "/viewer";
+            $.ajax({
+                method: "GET",
+                url: url,
+                success: function(data) {
+                    viewerColumn.html(data);
+                    attachEventListeners();
+                    updateHeight();
+                    navigator.trigger("IDEALS.FileNavigator.fileChanged");
+                },
+                error: function(data, status, xhr) {
+                    $("#file-navigator-viewer-column .spinner-border").hide();
+                    viewerColumn.html("<div id='file-navigator-viewer-error'>" +
+                        "<p>There was an error retrieving this file.</p></div>");
+                }
+            });
+        };
+
+        navigator.find("#file-navigator-thumbnail-column .thumbnail").on("keydown", function(e) {
+            const thumb = $(this);
+            switch (e.keyCode) {
+                case 37: // left arrow
+                    focus(thumb.prev());
+                    break;
+                case 39: // right arrow
+                    focus(thumb.next());
+                    break;
+            }
+        });
+
+        const thumbs = navigator.find("#file-navigator-thumbnail-column .thumbnail");
+        thumbs.on("click", function() {
+            const thumb = $(this);
+            focus(thumb);
+        });
+
+        // Select the primary bitstream, if one is set. Otherwise, select the
+        // first one.
+        let thumbToSelect = thumbs.filter("[data-primary=true]");
+        if (thumbToSelect.length < 1) {
+            thumbToSelect = thumbs.filter(":first");
         }
+        thumbToSelect.trigger("click");
     });
 
-    const thumbs = navigator.find("#file-navigator-thumbnail-column .thumbnail");
-    thumbs.on("click", function() {
-        const thumb = $(this);
-        focus(thumb);
-    });
-
-    // Select the primary bitstream, if one is set. Otherwise, select the
-    // first one.
-    let thumbToSelect = thumbs.filter("[data-primary=true]");
-    if (thumbToSelect.length < 1) {
-        thumbToSelect = thumbs.filter(":first");
-    }
-    thumbToSelect.trigger("click");
 };
 
 /**

app/assets/stylesheets/items.scss
@@ -12,8 +12,6 @@ body#show_item {
   #file-navigator {
     background-color: black;
     color: white;
-    display: grid;
-    grid-template-columns: 30% 70%;
     margin: 0 auto;
     width: 96%;
     #file-navigator-thumbnail-column {

app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
   include MetadataSubmission
   include Search
 
-  before_action :ensure_logged_in, except: [:index, :show]
+  before_action :ensure_logged_in, except: [:file_navigator, :index, :show]
   before_action :set_item, except: [:export, :index, :process_review, :review]
   before_action :authorize_item, except: [:export, :index, :process_review,
                                           :review]
@@ -165,6 +165,21 @@ class ItemsController < ApplicationController
   end
 
   ##
+  # Responds to `GET /items/:id/file_navigator` (XHR only)
+  #
+  def file_navigator
+    @content_bitstreams  = @item.bitstreams.
+      where(bundle: Bitstream::Bundle::CONTENT).
+      order("bitstreams.bundle_position", "LOWER(original_filename)").
+      select{ |b| policy(b).show? }
+    @other_bitstreams    = @item.bitstreams.
+      where("bundle != ?", Bitstream::Bundle::CONTENT).
+      order("bitstreams.bundle_position", "LOWER(original_filename)").
+      select{ |b| policy(b).show? }
+    render partial: "items/file_navigator"
+  end
+
+  ##
   # Responds to `GET /items`
   #
   def index
@@ -261,28 +276,6 @@ class ItemsController < ApplicationController
       render "show_buried", status: :gone
     when Item::Stages::WITHDRAWN
       render "show_withdrawn", status: :gone
-    else
-      show_approved
-    end
-  end
-
-  def show_approved
-    @content_bitstreams  = @item.bitstreams.
-      where(bundle: Bitstream::Bundle::CONTENT).
-      order("bitstreams.bundle_position", "LOWER(original_filename)").
-      select{ |b| policy(b).show? }
-    @other_bitstreams    = @item.bitstreams.
-      where("bundle != ?", Bitstream::Bundle::CONTENT).
-      order("bitstreams.bundle_position", "LOWER(original_filename)").
-      select{ |b| policy(b).show? }
-
-    respond_to do |format|
-      format.html do
-        render "show"
-      end
-      format.json do
-        render "show"
-      end
     end
   end
 

app/policies/item_policy.rb
@@ -123,6 +123,10 @@ class ItemPolicy < ApplicationPolicy
       reason:     "You must be a system or institution administrator." }
   end
 
+  def file_navigator
+    show
+  end
+
   def index
     AUTHORIZED_RESULT
   end

app/views/items/_file_navigator.html.haml
@@ -1,76 +1,68 @@
-- result = policy(@item).show_file_navigator
-- if !result[:authorized]
-  .alert.alert-secondary
-    %i.fa.fa-lock
-    = result[:reason]
+#file-navigator-thumbnail-column
+  #file-navigator-thumbnail-content
+    - tabindex = 0
+    - if @content_bitstreams.any?
+      .section-heading
+        Content Files
+      - @content_bitstreams.each do |bitstream|
+        .thumbnail{title:               bitstream.original_filename,
+                   "data-item-id":      bitstream.item_id,
+                   "data-bitstream-id": bitstream.id,
+                   "data-primary":      bitstream.primary ? "true" : "false",
+                   tabindex:            tabindex}
+          = representative_image_tag(bitstream,
+                                     region:         :square,
+                                     size:           512,
+                                     generate_async: true,
+                                     title:          bitstream.original_filename)
+          = bitstream.original_filename
+          - tabindex += 1
+    - if @other_bitstreams.any?
+      .section-heading
+        Other Files
+      - @other_bitstreams.each do |bitstream|
+        .thumbnail{title:               bitstream.original_filename,
+                   "data-item-id":      bitstream.item_id,
+                   "data-bitstream-id": bitstream.id,
+                   "data-primary":      bitstream.primary ? "true" : "false",
+                   tabindex:            tabindex}
+          = representative_image_tag(bitstream,
+                                     region:         :square,
+                                     size:           512,
+                                     generate_async: true,
+                                     title:          bitstream.original_filename)
+          = bitstream.original_filename
+          - tabindex += 1
+  - if @item.approved?
+    #file-navigator-thumbnail-column-footer
+      .btn-group.dropup{role: "group"}
+        %button#download-options{type:            "button",
+                                 class:           "btn btn-dark btn-sm dropdown-toggle",
+                                 "data-toggle":   "dropdown",
+                                 "aria-haspopup": "true",
+                                 "aria-expanded": "false"}
+          %i.fa.fa-download
+          Download Options
+        .dropdown-menu{"aria-labelledby": "download-options"}
+          %a.dropdown-item{href: item_bitstreams_path(@item, format: :zip)}
+            %i.fa.fa-download
+            Download All Files
+          - if policy(@item).download_counts?
+            %button.dropdown-item.download-counts{"data-item-id": @item.id,
+                                                  "data-target":  "#download-counts-modal",
+                                                  "data-toggle":  "modal",
+                                                  role:           "button"}
+              %i.fa.fa-chart-bar
+              Download Counts&hellip;
 
-- elsif @content_bitstreams.length + @other_bitstreams.length > 0
-  #file-navigator
-    #file-navigator-thumbnail-column
-      #file-navigator-thumbnail-content
-        - tabindex = 0
-        - if @content_bitstreams.any?
-          .section-heading
-            Content Files
-          - @content_bitstreams.each do |bitstream|
-            .thumbnail{title:               bitstream.original_filename,
-                       "data-item-id":      bitstream.item_id,
-                       "data-bitstream-id": bitstream.id,
-                       "data-primary":      bitstream.primary ? "true" : "false",
-                       tabindex:            tabindex}
-              = representative_image_tag(bitstream,
-                                         region:         :square,
-                                         size:           512,
-                                         generate_async: true,
-                                         title:          bitstream.original_filename)
-              = bitstream.original_filename
-              - tabindex += 1
-        - if @other_bitstreams.any?
-          .section-heading
-            Other Files
-          - @other_bitstreams.each do |bitstream|
-            .thumbnail{title:               bitstream.original_filename,
-                       "data-item-id":      bitstream.item_id,
-                       "data-bitstream-id": bitstream.id,
-                       "data-primary":      bitstream.primary ? "true" : "false",
-                       tabindex:            tabindex}
-              = representative_image_tag(bitstream,
-                                         region:         :square,
-                                         size:           512,
-                                         generate_async: true,
-                                         title:          bitstream.original_filename)
-              = bitstream.original_filename
-              - tabindex += 1
-      - if @item.approved?
-        #file-navigator-thumbnail-column-footer
-          .btn-group.dropup{role: "group"}
-            %button#download-options{type:            "button",
-                                     class:           "btn btn-dark btn-sm dropdown-toggle",
-                                     "data-toggle":   "dropdown",
-                                     "aria-haspopup": "true",
-                                     "aria-expanded": "false"}
-              %i.fa.fa-download
-              Download Options
-            .dropdown-menu{"aria-labelledby": "download-options"}
-              %a.dropdown-item{href: item_bitstreams_path(@item, format: :zip)}
-                %i.fa.fa-download
-                Download All Files
-              - if policy(@item).download_counts?
-                %button.dropdown-item.download-counts{"data-item-id": @item.id,
-                                                      "data-target":  "#download-counts-modal",
-                                                      "data-toggle":  "modal",
-                                                      role:           "button"}
-                  %i.fa.fa-chart-bar
-                  Download Counts&hellip;
+#file-navigator-viewer-column
+  -# bitstreams/_viewer.html is inserted here via XHR
+  = spinner
 
-    #file-navigator-viewer-column
-      -# bitstreams/_viewer.html is inserted here via XHR
-      = spinner
-
-  = render partial: "shared/ajax_modal",
-           locals: { id:    "download-counts-modal",
-                     title: "Download Counts (All Files)",
-                     size:  "lg" }
-  = render partial: "shared/ajax_modal",
-           locals: { id:    "edit-bitstream-modal",
-                     title: "Edit File" }
+= render partial: "shared/ajax_modal",
+         locals: { id:    "download-counts-modal",
+                   title: "Download Counts (All Files)",
+                   size:  "lg" }
+= render partial: "shared/ajax_modal",
+         locals: { id:    "edit-bitstream-modal",
+                   title: "Edit File" }

app/views/items/show.html.haml
@@ -1,6 +1,8 @@
 - provide :body_id, "show_item"
 - provide :title, @item.title
 
+= hidden_field_tag(:item_id, @item.id)
+
 = render partial: "show_button_group"
 
 - if @item.stage == Item::Stages::REJECTED
@@ -16,7 +18,15 @@
 
 .mb-3
 
-= render partial: "file_navigator"
+- result = policy(@item).show_file_navigator
+- if !result[:authorized]
+  .alert.alert-secondary
+    %i.fa.fa-lock
+    = result[:reason]
+- else
+  #file-navigator
+    = spinner
+    -# items/_file_navigator is inserted here via XHR
 
 - if @item.handle
   %h2.mt-5 Permalink

config/routes.rb
@@ -105,6 +105,8 @@ Rails.application.routes.draw do
           constraints: lambda { |request| request.xhr? }
     match "/edit-withdrawal", to: "items#edit_withdrawal", via: :get,
           constraints: lambda { |request| request.xhr? }
+    match "/file-navigator", to: "items#file_navigator", via: :get,
+          constraints: lambda { |request| request.xhr? }
     match "/reject", to: "items#reject", via: :patch
     match "/statistics", to: "items#statistics", via: :get,
           constraints: lambda { |request| request.xhr? }

commit a4dd893ac7b6c28b6bc45cdaa7ea644cf84f3f58

Fix the download counts modal, broken by 2950066a52890b4d28096579cba94d7d1d8268db

app/assets/javascripts/items.js
@@ -173,6 +173,41 @@ const FileNavigator = function() {
             thumbToSelect = thumbs.filter(":first");
         }
         thumbToSelect.trigger("click");
+
+        // Wire up the download counts button.
+        $(".download-counts").on("click", function() {
+            const id  = $(this).data("item-id");
+            const url = ROOT_URL + "/items/" + id + "/statistics";
+            $.get(url, function(data) {
+                const dl_counts_modal = $("#download-counts-modal");
+                dl_counts_modal.find(".modal-body").html(data);
+
+                const panel_content = $("#download-counts-xhr-content");
+                const form          = $("#download-counts-form");
+
+                const refreshContent = function() {
+                    const url = ROOT_URL + "/items/" + id + "/download-counts?" +
+                        form.serialize();
+                    $.get(url, function(data) {
+                        panel_content.prev().hide(); // hide the spinner
+                        panel_content.html(data);
+                        const canvas    = $("#download-chart");
+                        const chartData = $.parseJSON($("#chart-data").val());
+                        new IDEALS.Chart(canvas, chartData);
+                    });
+                };
+
+                refreshContent();
+
+                form.find("input[type=submit]").on("click", function() {
+                    // Remove existing content and show the spinner
+                    panel_content.empty();
+                    panel_content.prev().show(); // show the spinner
+                    refreshContent();
+                    return false;
+                });
+            });
+        });
     });
 
 };
@@ -307,40 +342,6 @@ const ItemView = function() {
         const url = ROOT_URL + "/items/" + id + "/edit-properties";
         $.get(url, function(data) {
             $("#edit-item-properties-modal .modal-body").html(data);
-        });
-    });
-    // Loads content into the Download Counts panel
-    $(".download-counts").on("click", function() {
-        const id  = $(this).data("item-id");
-        const url = ROOT_URL + "/items/" + id + "/statistics";
-        $.get(url, function(data) {
-            const dl_counts_modal = $("#download-counts-modal");
-            dl_counts_modal.find(".modal-body").html(data);
-
-            const panel_content = $("#download-counts-xhr-content");
-            const form          = $("#download-counts-form");
-
-            const refreshContent = function() {
-                const url = ROOT_URL + "/items/" + id + "/download-counts?" +
-                    form.serialize();
-                $.get(url, function(data) {
-                    panel_content.prev().hide(); // hide the spinner
-                    panel_content.html(data);
-                    const canvas    = $("#download-chart");
-                    const chartData = $.parseJSON($("#chart-data").val());
-                    new IDEALS.Chart(canvas, chartData);
-                });
-            };
-
-            refreshContent();
-
-            form.find("input[type=submit]").on("click", function() {
-                // Remove existing content and show the spinner
-                panel_content.empty();
-                panel_content.prev().show(); // show the spinner
-                refreshContent();
-                return false;
-            });
         });
     });
     $(".upload-item-files").on("click", function() {

app/assets/stylesheets/items.scss
@@ -11,10 +11,10 @@ body#show_item {
   /* N.B.: main pane heights are controlled in ItemViewer (items.js) */
   #file-navigator {
     background-color: black;
-    color: white;
     margin: 0 auto;
     width: 96%;
     #file-navigator-thumbnail-column {
+      color: white;
       #file-navigator-thumbnail-content {
         display: flex;
         flex-flow: row wrap;
@@ -68,6 +68,7 @@ body#show_item {
       }
     }
     #file-navigator-viewer-column {
+      color: white;
       height: 100%;
       #file-navigator-viewer-header {
         display: flex;