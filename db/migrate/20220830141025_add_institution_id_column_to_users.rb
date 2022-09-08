a5f1b168a49fd68ce7af514a12d8941c60df7d76
Alex Dolski <alexd@n-ary.net>
2022-09-08 16:02:01 -0500

    Fix migrations when there is no institution with a key of uiuc

--- db/migrate/20220830141025_add_institution_id_column_to_users.rb
+++ db/migrate/20220830141025_add_institution_id_column_to_users.rb
@@ -2,7 +2,7 @@ class AddInstitutionIdColumnToUsers < ActiveRecord::Migration[7.0]
   def up
     add_column :users, :institution_id, :bigint
     id = execute("SELECT id FROM institutions WHERE key = 'uiuc';")[0]['id']
-    execute "UPDATE users SET institution_id = #{id} WHERE email LIKE '%illinois.edu' OR email LIKE '%uiuc.edu';"
+    execute "UPDATE users SET institution_id = #{id} WHERE email LIKE '%illinois.edu' OR email LIKE '%uiuc.edu';" if id
 
     add_foreign_key :users, :institutions, on_update: :cascade,
                     on_delete: :restrict
--- db/migrate/20220906155941_add_institution_id_to_imports.rb
+++ db/migrate/20220906155941_add_institution_id_to_imports.rb
@@ -2,6 +2,7 @@ class AddInstitutionIdToImports < ActiveRecord::Migration[7.0]
   def change
     add_column :imports, :institution_id, :bigint
     id = execute("SELECT id FROM institutions WHERE key = 'uiuc';")[0]['id']
+    id = execute("SELECT id FROM institutions ORDER BY id;")[0]['id'] unless id
     execute "UPDATE imports SET institution_id = #{id};"
     change_column_null :imports, :institution_id, false
     add_foreign_key :imports, :institutions, on_update: :cascade, on_delete: :cascade
