87d96406274a69b6c787e2a27345e974ad54c9cf
Alex Dolski <alexd@n-ary.net>
2022-11-23 10:22:05 -0600

    Make a test more reliable

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -5,11 +5,11 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
   test "disabled users are logged out" do
     user = users(:norights)
     log_in_as(user)
-    get items_path
+    get root_path
     assert_response :ok
 
     user.update!(enabled: false)
-    get items_path
+    get root_path
     assert_redirected_to root_path
   end
 
