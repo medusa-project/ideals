c1c5225fab8e8f1376886b0ef8d3ec088fd7ebde
Alex Dolski <alexd@n-ary.net>
2022-11-28 15:59:18 -0600

    Make the login panel NetID vs. non-NetID distinction more intuitive

--- app/assets/javascripts/ideals.js
+++ app/assets/javascripts/ideals.js
@@ -779,6 +779,21 @@ const IDEALS = {
         const ROOT_URL = $("input[name=root_url]").val();
         const modal    = $("#login-modal");
         const flash    = modal.find(".alert.login-status");
+        modal.find("input[name=auth_key]").on("keyup", function() {
+            const submitButton = modal.find("button[type=submit]");
+            if ($(this).val().endsWith("@illinois.edu")) {
+                flash.removeClass("alert-success")
+                    .addClass("alert-danger")
+                    .text("This login method will not work with an @illinois.edu " +
+                        "email addresses. Use the \"Log in with Illinois " +
+                        "NetID\" button instead.")
+                    .show();
+                submitButton.prop("disabled", true);
+            } else {
+                flash.hide();
+                submitButton.prop("disabled", false);
+            }
+        });
         modal.find("button[type=submit]").on("click", function(event) {
             event.preventDefault();
             $.ajax({
