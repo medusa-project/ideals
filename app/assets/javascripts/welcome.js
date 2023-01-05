b0c609d066ad267d5660f505249db156eceedeff
Alex Dolski <alexd@n-ary.net>
2023-01-05 11:23:43 -0600

    Fix behavior of overlapping map points

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -5,8 +5,12 @@ const GlobalLandingView = function() {
     const r                 = $(".institution-point").attr("r");
     const institution_links = $(".institution");
     institution_links.on("mouseenter", function() {
-        const key   = $(this).data("key");
-        const point = $("#" + key + "-point");
+        const key          = $(this).data("key");
+        const point        = $("#" + key + "-point");
+        const last_sibling = point.siblings(":last");
+        // Move the point past its last sibling to prevent it from being
+        // obscured by overlapping points.
+        point.insertAfter(last_sibling);
         point.addClass("selected");
         point.animate({ r: r * 1.5 }, 150);
     }).on("mouseleave", function() {
