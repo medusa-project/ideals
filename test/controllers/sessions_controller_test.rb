medusa-project/ideals

commit d5bed239c5e0f4898b4c6e5ffb175c2753e202bd

unauthorized() redirects instead of returning HTTP 401

app/controllers/sessions_controller.rb
@@ -17,6 +17,17 @@ class SessionsController < ApplicationController
   end
 
   ##
+  # Handles callbacks from the auth provider. (In development this is the
+  # OmniAuth development strategy, and in demo/production it's the Shibboleth
+  # SP.) It is responsible for taking the UID from the authentication hash,
+  # translating it into a {User}, and setting the user's ID in the session.
+  #
+  # This method will only have been called upon successful authentication--
+  # never upon failure. However, only {User#enabled enabled users} whose
+  # {User#institution owning institution} matches the request institution are
+  # allowed to log in. {LocalUser}s also must have an associated activated
+  # {LocalIdentity}.
+  #
   # Responds to `GET/POST /auth/:provider/callback`.
   #
   def create
@@ -47,16 +58,20 @@ class SessionsController < ApplicationController
   end
 
   def unauthorized
-    render plain: "401 Unauthorized", status: :unauthorized
+    flash['error'] = "You are not authorized to log in. "\
+                     "If this problem persists, please contact us."
+    redirect_to return_url, allow_other_host: true
   end
 
-  protected
+
+  private
 
   def return_url
-    session[:login_return_url] || root_url
+    session[:login_return_url] || current_institution&.scope_url || root_url
   end
 
   def shibboleth_login_path(host)
     "/Shibboleth.sso/Login?target=https://#{host}/auth/shibboleth/callback"
   end
+
 end

test/controllers/sessions_controller_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
 
+  setup do
+    @institution = institutions(:example)
+    host! @institution.fqdn
+  end
+
   teardown do
     log_out
   end
@@ -13,37 +18,37 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
         auth_key: "bogus@example.edu",
         password: "WRONG"
     }
-    assert_redirected_to "http://www.example.com/auth/failure?message=invalid_credentials&strategy=identity"
+    assert_redirected_to @institution.scope_url + "/auth/failure?message=invalid_credentials&strategy=identity"
   end
 
-  test "create() with a non-activated user responds with HTTP 401" do
+  test "create() with a non-activated user redirects to the return URL" do
     user = users(:example)
     user.identity.update_attribute(:activated, false)
     post "/auth/identity/callback", params: {
         auth_key: user.email,
         password: "password"
     }
-    assert_response :unauthorized
+    assert_redirected_to @institution.scope_url
   end
 
-  test "create() with a disabled user responds with HTTP 401" do
+  test "create() with a disabled user redirects to the return URL" do
     user = users(:example)
     user.update!(enabled: false)
     post "/auth/identity/callback", params: {
       auth_key: user.email,
       password: "password"
     }
-    assert_response :unauthorized
+    assert_redirected_to @institution.scope_url
   end
 
-  test "create() with user of different institution responds with HTTP 401" do
-    host! institutions(:southwest).fqdn
+  test "create() with user of different institution redirects to the return
+  URL" do
     user = users(:northeast)
     post "/auth/identity/callback", params: {
       auth_key: user.email,
       password: "password"
     }
-    assert_response :unauthorized
+    assert_redirected_to @institution.scope_url
   end
 
   test "create() with valid credentials redirects to root URL" do
@@ -53,7 +58,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
         auth_key: user.email,
         password: "password"
     }
-    assert_redirected_to root_url
+    assert_redirected_to @institution.scope_url
   end
 
   test "create() with valid credentials sets the user's auth hash" do
@@ -98,7 +103,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
 
   test "new_netid() redirects to netid login path" do
     get netid_login_path
-    assert_redirected_to "http://www.example.com/auth/developer"
+    assert_redirected_to @institution.scope_url + "/auth/developer"
   end
 
 end

commit c91728634845255f023c769822ce5c1421f59478

Return HTTP 401 for failed logins instead of redirecting (fixes a problem introduced in  d5bed239c5e0f4898b4c6e5ffb175c2753e202bd)

app/controllers/sessions_controller.rb
@@ -57,10 +57,30 @@ class SessionsController < ApplicationController
                 allow_other_host: true)
   end
 
-  def unauthorized
-    flash['error'] = "You are not authorized to log in. "\
-                     "If this problem persists, please contact us."
-    redirect_to return_url, allow_other_host: true
+  ##
+  # Handles omniauth-identity authentication failures.
+  #
+  # Responds to `GET /auth/failure`.
+  #
+  def auth_failed
+    message = "Login failed: incorrect username and/or password."
+    if request.xhr?
+      render plain: message, status: :unauthorized
+    else
+      flash['error'] = message
+      redirect_to return_url, allow_other_host: true
+    end
+  end
+
+  def unauthorized # TODO: this should be private
+    message = "You are not authorized to log in. "\
+              "If this problem persists, please contact us."
+    if request.xhr?
+      render plain: message, status: :forbidden
+    else
+      flash['error'] = message
+      redirect_to return_url, allow_other_host: true
+    end
   end
 
 

config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # authentication routes
   match "/auth/:provider/callback", to: "sessions#create", via: [:get, :post]
-  match "/auth/failure", to: "sessions#unauthorized", as: :unauthorized, via: [:get, :post]
+  match "/auth/failure", to: "sessions#auth_failed", as: :auth_failed, via: [:get, :post]
   match "/logout", to: "sessions#destroy", as: :logout, via: :all
   match "/netid-login", to: "sessions#new_netid", as: :netid_login, via: [:get, :post]
 

test/controllers/sessions_controller_test.rb
@@ -31,6 +31,16 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to @institution.scope_url
   end
 
+  test "create() via XHR with a non-activated user returns HTTP 403" do
+    user = users(:example)
+    user.identity.update_attribute(:activated, false)
+    post "/auth/identity/callback", params: {
+      auth_key: user.email,
+      password: "password"
+    }, xhr: true
+    assert_response :forbidden
+  end
+
   test "create() with a disabled user redirects to the return URL" do
     user = users(:example)
     user.update!(enabled: false)
@@ -39,6 +49,16 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
       password: "password"
     }
     assert_redirected_to @institution.scope_url
+  end
+
+  test "create() via XHR with a disabled user returns HTTP 403" do
+    user = users(:example)
+    user.update!(enabled: false)
+    post "/auth/identity/callback", params: {
+      auth_key: user.email,
+      password: "password"
+    }, xhr: true
+    assert_response :forbidden
   end
 
   test "create() with user of different institution redirects to the return
@@ -51,6 +71,15 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to @institution.scope_url
   end
 
+  test "create() via XHR with user of different institution returns HTTP 403" do
+    user = users(:northeast)
+    post "/auth/identity/callback", params: {
+      auth_key: user.email,
+      password: "password"
+    }, xhr: true
+    assert_response :forbidden
+  end
+
   test "create() with valid credentials redirects to root URL" do
     user = users(:example)
     user.institution.update!(default: true)
@@ -58,6 +87,16 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
         auth_key: user.email,
         password: "password"
     }
+    assert_redirected_to @institution.scope_url
+  end
+
+  test "create() via XHR with valid credentials redirects to root URL" do
+    user = users(:example)
+    user.institution.update!(default: true)
+    post "/auth/identity/callback", params: {
+      auth_key: user.email,
+      password: "password"
+    }, xhr: true
     assert_redirected_to @institution.scope_url
   end
 