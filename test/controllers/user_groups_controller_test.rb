673d3aee192a217f61c5f629a147360520cc9dbe
Alex Dolski <alexd@n-ary.net>
2023-05-05 16:59:56 -0500

    Fix broken tests

--- test/controllers/user_groups_controller_test.rb
+++ test/controllers/user_groups_controller_test.rb
@@ -326,75 +326,39 @@ class UserGroupsControllerTest < ActionDispatch::IntegrationTest
     assert_response :ok
   end
 
-  # edit_local_users()
+  # edit_users()
 
-  test "edit_local_users() returns HTTP 404 for unscoped requests" do
+  test "edit_users() returns HTTP 404 for unscoped requests" do
     host! ::Configuration.instance.main_host
     group = user_groups(:uiuc)
-    get user_group_edit_local_users_path(group), xhr: true
+    get user_group_edit_users_path(group), xhr: true
     assert_response :not_found
   end
 
-  test "edit_local_users() returns HTTP 403 for logged-out users" do
+  test "edit_users() returns HTTP 403 for logged-out users" do
     group = user_groups(:uiuc)
-    get user_group_edit_local_users_path(group), xhr: true
+    get user_group_edit_users_path(group), xhr: true
     assert_response :forbidden
   end
 
-  test "edit_local_users() returns HTTP 403 for unauthorized users" do
+  test "edit_users() returns HTTP 403 for unauthorized users" do
     log_in_as(users(:uiuc))
     group = user_groups(:uiuc)
-    get user_group_edit_local_users_path(group), xhr: true
+    get user_group_edit_users_path(group), xhr: true
     assert_response :forbidden
   end
 
-  test "edit_local_users() returns HTTP 404 for non-XHR requests" do
+  test "edit_users() returns HTTP 404 for non-XHR requests" do
     log_in_as(users(:uiuc_sysadmin))
     group = user_groups(:uiuc)
-    get user_group_edit_local_users_path(group)
+    get user_group_edit_users_path(group)
     assert_response :not_found
   end
 
-  test "edit_local_users() returns HTTP 200 for XHR requests" do
+  test "edit_users() returns HTTP 200 for XHR requests" do
     log_in_as(users(:uiuc_sysadmin))
     group = user_groups(:uiuc)
-    get user_group_edit_local_users_path(group), xhr: true
-    assert_response :ok
-  end
-
-  # edit_shibboleth_users()
-
-  test "edit_shibboleth_users() returns HTTP 404 for unscoped requests" do
-    host! ::Configuration.instance.main_host
-    group = user_groups(:uiuc)
-    get user_group_edit_shibboleth_users_path(group), xhr: true
-    assert_response :not_found
-  end
-
-  test "edit_shibboleth_users() returns HTTP 403 for logged-out users" do
-    group = user_groups(:uiuc)
-    get user_group_edit_shibboleth_users_path(group), xhr: true
-    assert_response :forbidden
-  end
-
-  test "edit_shibboleth_users() returns HTTP 403 for unauthorized users" do
-    log_in_as(users(:uiuc))
-    group = user_groups(:uiuc)
-    get user_group_edit_shibboleth_users_path(group), xhr: true
-    assert_response :forbidden
-  end
-
-  test "edit_shibboleth_users() returns HTTP 404 for non-XHR requests" do
-    log_in_as(users(:uiuc_sysadmin))
-    group = user_groups(:uiuc)
-    get user_group_edit_shibboleth_users_path(group)
-    assert_response :not_found
-  end
-
-  test "edit_shibboleth_users() returns HTTP 200 for XHR requests" do
-    log_in_as(users(:uiuc_sysadmin))
-    group = user_groups(:uiuc)
-    get user_group_edit_shibboleth_users_path(group), xhr: true
+    get user_group_edit_users_path(group), xhr: true
     assert_response :ok
   end
 
