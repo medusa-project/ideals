918d679d0968db815f7ec162356ef8567dcd8da5
Alex Dolski <alexd@n-ary.net>
2023-05-08 09:03:14 -0500

    Override the auth_hash setter to work around a serialization bug involving the omniauth-saml auth hash

--- app/models/login.rb
+++ app/models/login.rb
@@ -19,4 +19,16 @@ class Login < ApplicationRecord
 
   serialize :auth_hash, JSON
 
+  def auth_hash=(auth)
+    # By default, omniauth-saml's auth hash is not serializable--it will raise
+    # a StackLevelTooDeep error which is probably a bug. So we will discard the
+    # parts that are causing problems (which are fortunately irrelevant).
+    if auth[:provider] == "saml"
+      auth = auth.deep_symbolize_keys
+      auth[:extra][:raw_info]        = auth[:extra][:raw_info].attributes
+      auth[:extra][:response_object] = nil
+    end
+    super(auth)
+  end
+
 end
