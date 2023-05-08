8eb57985bde0db6c4f35f5537b76d88f75156211
Alex Dolski <alexd@n-ary.net>
2023-05-08 16:44:13 -0500

    auth_hash=() populates auth_method

--- app/models/login.rb
+++ app/models/login.rb
@@ -3,6 +3,9 @@
 ##
 # Encapsulates a {User} login event.
 #
+# N.B.: it is debatable whether this should be more strongly related to
+# {Event}--perhaps a subclass.
+#
 # # Attributes
 #
 # * `auth_hash`   Serialized OmniAuth hash.
@@ -23,11 +26,21 @@ class Login < ApplicationRecord
     # By default, omniauth-saml's auth hash is not serializable--it will raise
     # a StackLevelTooDeep error which is probably a bug. So we will discard the
     # parts that are causing problems (which are fortunately irrelevant).
-    if auth[:provider] == "saml"
+    if auth[:provider] == "saml" && auth.dig(:extra, :raw_info)
       auth = auth.deep_symbolize_keys
       auth[:extra][:raw_info]        = auth[:extra][:raw_info].attributes
       auth[:extra][:response_object] = nil
     end
+
+    case auth[:provider]
+    when "shibboleth", "developer"
+      self.auth_method = User::AuthMethod::SHIBBOLETH
+    when "saml"
+      self.auth_method = User::AuthMethod::OPENATHENS
+    when "identity"
+      self.auth_method = User::AuthMethod::LOCAL
+    end
+
     super(auth)
   end
 
--- test/models/login_test.rb
+++ test/models/login_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class LoginTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @instance = Login.new
+  end
+
+  # auth_hash=()
+
+  test "auth_hash=() sets the auth method" do
+    @instance.auth_hash = { provider: "saml" }
+    assert_equal User::AuthMethod::OPENATHENS, @instance.auth_method
+
+    @instance.auth_hash = { provider: "shibboleth" }
+    assert_equal User::AuthMethod::SHIBBOLETH, @instance.auth_method
+
+    @instance.auth_hash = { provider: "developer" }
+    assert_equal User::AuthMethod::SHIBBOLETH, @instance.auth_method
+
+    @instance.auth_hash = { provider: "identity" }
+    assert_equal User::AuthMethod::LOCAL, @instance.auth_method
+  end
+
 end
