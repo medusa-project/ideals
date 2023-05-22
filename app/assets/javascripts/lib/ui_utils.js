medusa-project/ideals

commit ab11d57f5768fe635e2f17f9aa7b651a61f45f7e

Replace regex ^, $ beginning/end matchers with \A, \z

app/assets/javascripts/submissions.js
@@ -356,7 +356,7 @@ const SubmissionForm = function() {
         metadataForm.find("[name=year]").on("input", function () {
             const input = $(this);
             input.val(input.val().replace(/[^\d]/g, "")); // only numbers
-            input.val(input.val().replace(/^0/g, ""));    // years may not start with 0
+            input.val(input.val().replace(/\A0/g, ""));    // years may not start with 0
         });
     }
 

app/assets/javascripts/ideals.js
@@ -204,7 +204,7 @@ const IDEALS = {
     Chart: function(canvas, chart_data, color) {
         // X axis labels
         const labels = $.map(chart_data, function(n, i) {
-            const regex = /^(\d+)-(\d+)-(\d+)/;
+            const regex = /\A(\d+)-(\d+)-(\d+)/;
             const matches = regex.exec(n.month);
             return matches[2] + "/" + matches[1];
         });

app/helpers/application_helper.rb
@@ -467,7 +467,7 @@ module ApplicationHelper
             # TODO: this is an ugly hack because the FA SVGs referenced in
             # formats.yml are from an earlier version of FA
             icon = "file-alt" if icon == "file-text-o"
-            icon = "far fa-#{icon.gsub(/-o$/, "")}"
+            icon = "far fa-#{icon.gsub(/-o\z/, "")}"
           end
         end
       end

app/models/ascribed_element.rb
@@ -45,20 +45,20 @@ class AscribedElement < ApplicationRecord
     if s.match?(/\dT\d{2}:/)
       return Date.parse(s)
     # MM/DD/YY or MM/DD/YYYY
-    elsif s.match?(/^\d{1,2}\/\d{1,2}\/\d{2,4}/)
+    elsif s.match?(/\A\d{1,2}\/\d{1,2}\/\d{2,4}/)
       parts = s.split("/")
       parts[2] = "19#{parts[2]}" if parts[2].length == 2
       return Date.new(parts[2].to_i, parts[0].to_i, parts[1].to_i)
     # YYYY-MM-DD
-    elsif s.match?(/^\d{4}-\d{2}-\d{2}/)
+    elsif s.match?(/\A\d{4}-\d{2}-\d{2}/)
       parts = s.split("-")
       return Date.new(parts[0].to_i, parts[1].to_i, parts[2].to_i)
     # YYYY-MM
-    elsif s.match?(/^\d{4}-\d{2}/)
+    elsif s.match?(/\A\d{4}-\d{2}/)
       parts = s.split("-")
       return Date.new(parts[0].to_i, parts[1].to_i)
     # YYYY
-    elsif s.match?(/^\d{4}/)
+    elsif s.match?(/\A\d{4}/)
       return Date.new(s.to_i)
     # Mon DD YYYY, DD-Mon-YY, Mon YYYY
     elsif s.match?(/[A-Za-z]+ \d{1,2},? \d{4}/) ||

app/models/persistent_store.rb
@@ -165,7 +165,7 @@ class PersistentStore
       raise ArgumentError, "One of the source arguments must be provided."
     end
     unless institution_key
-      result = key.match(/^institutions\/(\w+)/i)
+      result = key.match(/\Ainstitutions\/(\w+)/i)
       institution_key = result.captures[0] if result
     end
     if institution_key

app/util/color_utils.rb
@@ -6,11 +6,11 @@ class ColorUtils
   #
   def self.css_color?(string)
     return false if string.blank?
-    string.match?(/^#[A-Fa-f0-9]{3}$/) ||                                           # 3-digit hex
-      string.match?(/^#[A-Fa-f0-9]{6}$/) ||                                         # 6-digit hex
-      string.match?(/^rgb\(\d{1,3}%?, *\d{1,3}%?, *\d{1,3}%?\)$/) ||             # rgb()
-      string.match?(/^rgba\(\d{1,3}%?, *\d{1,3}%?, *\d{1,3}%?, *\d?.?\d?\)$/) || # rgba()
-      string.match?(/^[a-z]+$/)                                                  # color name
+    string.match?(/\A#[A-Fa-f0-9]{3}\z/) ||                                        # 3-digit hex
+      string.match?(/\A#[A-Fa-f0-9]{6}\z/) ||                                      # 6-digit hex
+      string.match?(/\Argb\(\d{1,3}%?, *\d{1,3}%?, *\d{1,3}%?\)\z/) ||             # rgb()
+      string.match?(/\Argba\(\d{1,3}%?, *\d{1,3}%?, *\d{1,3}%?, *\d?.?\d?\)\z/) || # rgba()
+      string.match?(/\A[a-z]+\z/)                                                  # color name
   end
 
   ##

app/util/saf_importer.rb
@@ -206,7 +206,7 @@ class SafImporter
 
           # Add metadata corresponding to any other metadata files.
           item_object_keys = store.objects(key_prefix: item_dir)
-          item_object_keys.map(&:key).select{ |k| k.match(/\/metadata_.+.xml$/) }.each do |metadata_key|
+          item_object_keys.map(&:key).select{ |k| k.match(/\/metadata_.+.xml\z/) }.each do |metadata_key|
             content = store.get_object(key: metadata_key).read
             add_metadata(item:                   item,
                          metadata_relative_path: metadata_key,

app/util/time_utils.rb
@@ -38,14 +38,14 @@ class TimeUtils
                      September October November December)
     iso_str = year = month = day = nil
     # Check for "Month DD YYYY" or "Month DD, YYYY"
-    result = string.match(/(#{month_names.join("|")}) (\d{1,2}),? (\d{4})$/)
+    result = string.match(/(#{month_names.join("|")}) (\d{1,2}),? (\d{4})\z/)
     if result
       month = (month_names.index(result[1]) + 1).to_s.rjust(2, "0")
       day   = result[2].rjust(2, "0")
       year  = result[3]
     end
     # Check for "Month YYYY" or "Month, YYYY"
-    result = string.match(/(#{month_names.join("|")}),? ?(\d{4})$/)
+    result = string.match(/(#{month_names.join("|")}),? ?(\d{4})\z/)
     if result
       month = (month_names.index(result[1]) + 1).to_s.rjust(2, "0")
       year  = result[2]

commit de63819cfe7d5b72b9d402f36c2b8af1c54d592e

Fix regex start anchors broken by ab11d57f5768fe635e2f17f9aa7b651a61f45f7e

app/assets/javascripts/lib/ui_utils.js
@@ -22,7 +22,7 @@ IDEALS.UIUtils.KEY_DELAY = 1500;
 IDEALS.UIUtils.Chart = function(canvas, chart_data, color) {
     // X axis labels
     const labels = $.map(chart_data, function(n, i) {
-        const regex   = /\A(\d+)-(\d+)-(\d+)/;
+        const regex   = /^(\d+)-(\d+)-(\d+)/;
         const matches = regex.exec(n.month);
         return matches[2] + "/" + matches[1];
     });

app/assets/javascripts/submissions.js
@@ -350,7 +350,7 @@ const SubmissionForm = function() {
         metadataForm.find("[name=year]").on("input", function () {
             const input = $(this);
             input.val(input.val().replace(/[^\d]/g, "")); // only numbers
-            input.val(input.val().replace(/\A0/g, ""));    // years may not start with 0
+            input.val(input.val().replace(/^0/g, ""));    // years may not start with 0
         });
     }
 