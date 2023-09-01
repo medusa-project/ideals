606964f7807eb6117d019f93758e95b204e8e579
Alex Dolski <alexd@n-ary.net>
2023-09-01 17:03:11 -0500

    Improve safety

--- db/migrate/20230808202216_add_institution_id_column_to_events.rb
+++ db/migrate/20230808202216_add_institution_id_column_to_events.rb
@@ -3,6 +3,22 @@ class AddInstitutionIdColumnToEvents < ActiveRecord::Migration[7.0]
     add_column :events, :institution_id, :bigint
     add_foreign_key :events, :institutions, on_update: :cascade, on_delete: :cascade
 
+    # Logins
+    results = execute("SELECT DISTINCT e.login_id, u.institution_id
+                       FROM events e
+                       LEFT JOIN logins l ON l.id = e.login_id
+                       LEFT JOIN users u ON l.user_id = u.id
+                       WHERE e.login_id IS NOT NULL;")
+    map = {}
+    results.each do |row|
+      map[row['institution_id']] = [] if map[row['institution_id']].nil?
+      map[row['institution_id']] << row['login_id']
+    end
+    map.each do |institution_id, login_ids|
+      execute("UPDATE events SET institution_id = #{institution_id}
+               WHERE login_id IN (#{login_ids.join(",")});") if institution_id
+    end
+
     # Items
     results = execute("SELECT DISTINCT e.item_id, i.institution_id
                        FROM events e
@@ -32,22 +48,6 @@ class AddInstitutionIdColumnToEvents < ActiveRecord::Migration[7.0]
     map.each do |institution_id, bitstream_ids|
       execute("UPDATE events SET institution_id = #{institution_id}
                WHERE bitstream_id IN (#{bitstream_ids.join(",")});")
-    end
-
-    # Logins
-    results = execute("SELECT DISTINCT e.login_id, u.institution_id
-                       FROM events e
-                       LEFT JOIN logins l ON l.id = e.login_id
-                       LEFT JOIN users u ON l.user_id = u.id
-                       WHERE e.login_id IS NOT NULL;")
-    map = {}
-    results.each do |row|
-      map[row['institution_id']] = [] if map[row['institution_id']].nil?
-      map[row['institution_id']] << row['login_id']
-    end
-    map.each do |institution_id, login_ids|
-      execute("UPDATE events SET institution_id = #{institution_id}
-               WHERE login_id IN (#{login_ids.join(",")});")
     end
 
     add_index :events, :institution_id
