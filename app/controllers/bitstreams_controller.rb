medusa-project/ideals

commit cdb99299844c78f7d09a5c42e87134b7660fb3ea

Convert Office formats to PDF in order to use the PDF.js viewer

app/controllers/bitstreams_controller.rb
@@ -19,6 +19,9 @@
 #    as it ties up a request connection.
 # 2. By redirecting to a presigned S3 URL using {object}.
 #
+# Also, for PDFs and PDF-like formats, PDF format can be requested by adding
+# a `.pdf` extension to the {show} URL.
+#
 # Bitstreams do not have public URLs because they do not have public-read ACLs.
 # (Some non-bitstream objects in the application bucket do have such ACLs, such
 # as the theme images.)
@@ -247,7 +250,22 @@ class BitstreamsController < ApplicationController
   # Responds to `GET /items/:item_id/bitstreams/:id`
   #
   def show
-    render formats: :json
+    respond_to do |format|
+      format.pdf do
+        format = @bitstream.format
+        if format.media_type == "application/pdf"
+          data
+        elsif format.derivative_generator == "libreoffice"
+          redirect_to @bitstream.derivative_pdf_url, allow_other_host: true
+        else
+          render plain: "Unable to provide a PDF representation of this bitstream.",
+                 status: :not_found
+        end
+      end
+      format.any do
+        render formats: :json
+      end
+    end
   end
 
   ##
@@ -309,10 +327,8 @@ class BitstreamsController < ApplicationController
   def download_content_disposition
     utf8_filename  = @bitstream.filename
     ascii_filename = utf8_filename.gsub(/[^[:ascii:]]*/, '_')
-    # N.B.: CGI.escape() inserts "+" instead of "%20" which Chrome interprets
-    # literally.
     "attachment; filename=\"#{ascii_filename.gsub('"', "'")}\"; "\
-      "filename*=UTF-8''#{ERB::Util.url_encode(utf8_filename)}"
+      "filename*=UTF-8''#{StringUtils.url_encode(utf8_filename)}"
   end
 
   def rescue_invalid_range

app/helpers/application_helper.rb
@@ -881,9 +881,9 @@ module ApplicationHelper
     if item.kind_of?(Item)
       bs = item.representative_bitstream
       if bs&.has_representative_image?
-        url = bs.derivative_url(region:         :square,
-                                size:           512,
-                                generate_async: true)
+        url = bs.derivative_image_url(region:         :square,
+                                      size:           512,
+                                      generate_async: true)
         if url
           return raw("<img src=\"#{url}\" alt=\"Thumbnail for #{item.title}\"/>")
         end

app/helpers/bitstreams_helper.rb
@@ -9,10 +9,11 @@ module BitstreamsHelper
   # {FileFormat} registry.
   #
   # @param bitstream [Bitstream]
-  # @param region [Symbol] See {Bitstream#derivative_url}.
-  # @param size [Integer] See {Bitstream#derivative_url}. This is the literal
-  #                       size of the image; the `img` tag has no `width`/
-  #                       `height` attribute and must be sized via CSS.
+  # @param region [Symbol] See {Bitstream#derivative_image_url}.
+  # @param size [Integer] See {Bitstream#derivative_image_url}. This is the
+  #                       literal size of the image; the `img` tag has no
+  #                       `width` or `height` attribute and must be sized via
+  #                       CSS.
   # @param generate_async [Boolean]
   # @param attrs [Hash] Additional tag attributes.
   # @return [String] HTML `img` tag.
@@ -25,9 +26,9 @@ module BitstreamsHelper
     path = nil
     if bitstream.has_representative_image?
       begin
-        path = bitstream.derivative_url(region:         region,
-                                        size:           size,
-                                        generate_async: generate_async)
+        path = bitstream.derivative_image_url(region:         region,
+                                              size:           size,
+                                              generate_async: generate_async)
         svg  = false
       rescue
         # The object may not exist, or something else is wrong, but we can't
@@ -127,7 +128,11 @@ module BitstreamsHelper
     # cross-domain requests. See: https://stackoverflow.com/a/69342595
     # The commit history of public/pdfjs/web/viewer.js should reveal the
     # change.
-    bitstream_url = bitstream.presigned_download_url(content_disposition: "inline")
+    if bitstream.format.derivative_generator == "libreoffice"
+      bitstream_url = item_bitstream_url(bitstream.item, bitstream, format: :pdf)
+    else
+      bitstream_url = bitstream.presigned_download_url(content_disposition: "inline")
+    end
     viewer_url    = asset_path("/pdfjs/web/viewer.html?file=" + CGI.escape(bitstream_url))
     html          = "<iframe src=\"#{viewer_url}\" id=\"pdfjs-pdf-viewer\" "\
                             "height=\"100%\" width=\"100%\" "\

app/jobs/generate_derivative_image_job.rb
@@ -6,7 +6,7 @@ class GenerateDerivativeImageJob < ApplicationJob
   # @param args [Array] Four-element array with [Bitstream] at position 0 and
   #                     region, size, and format elements in the remaining
   #                     positions in the same format as the arguments to
-  #                     {Bitstream#generate_derivative}.
+  #                     {Bitstream#generate_image_derivative}.
   #
   def perform(*args)
     bs     = args[0]
@@ -22,7 +22,8 @@ class GenerateDerivativeImageJob < ApplicationJob
                                        "derivative image for #{bs.filename} "\
                                        "(item ID #{bs.item_id})")
     begin
-      bs.send(:generate_derivative, region: region, size: size, format: format)
+      bs.send(:generate_image_derivative,
+              region: region, size: size, format: format)
     rescue => e
       task.fail(detail:    e.message,
                 backtrace: e.backtrace)

app/models/bitstream.rb
@@ -57,9 +57,9 @@
 # # Derivative images
 #
 # The application supports image previews for some file types. If the return
-# value of {has_representative_image?} is `true`, {derivative_url} can be used
-# to obtain the URL of a derivative image with the given characteristics. The
-# URL points to an image in the application S3 bucket, which is generated
+# value of {has_representative_image?} is `true`, {derivative_image_url} can be
+# used to obtain the URL of a derivative image with the given characteristics.
+# The URL points to an image in the application S3 bucket, which is generated
 # on-the-fly and cached.
 #
 # # Full text
@@ -431,23 +431,43 @@ class Bitstream < ApplicationRecord
   # @return [String] Public URL for a derivative image with the given
   #                  characteristics. If no such image exists, it is generated
   #                  automatically.
-  # @raises [Aws::S3::Errors::NotFound]
   #
-  def derivative_url(region: :full, size:, generate_async: false)
+  def derivative_image_url(region: :full, size:, generate_async: false)
     unless has_representative_image?
       raise "Derivatives are not supported for this format."
     end
     store = PersistentStore.instance
-    key   = derivative_key(region: region, size: size, format: :jpg)
+    key   = derivative_image_key(region: region, size: size, format: :jpg)
     unless store.object_exists?(key: key)
       if generate_async
         GenerateDerivativeImageJob.perform_later(self, region, size, :jpg)
         return nil
       else
-        generate_derivative(region: region, size: size, format: :jpg)
+        generate_image_derivative(region: region, size: size, format: :jpg)
       end
     end
     store.presigned_download_url(key: key, expires_in: 1.hour.to_i)
+  end
+
+  ##
+  # @return [String] Public URL for a derivative PDF with the given
+  #                  characteristics. If no such image exists, it is generated
+  #                  automatically.
+  #
+  def derivative_pdf_url
+    store  = PersistentStore.instance
+    expiry = 1.hour.to_i
+    format = self.format
+    if format.media_type == "application/pdf"
+      return store.presigned_download_url(key:        self.effective_key,
+                                          expires_in: expiry)
+    elsif format.derivative_generator != "libreoffice"
+      raise "This instance cannot be converted to PDF."
+    end
+
+    key = derivative_pdf_key
+    generate_pdf_derivative unless store.object_exists?(key: key)
+    store.presigned_download_url(key: key, expires_in: expiry)
   end
 
   ##
@@ -474,8 +494,9 @@ class Bitstream < ApplicationRecord
     rescue => e
       tempfile.unlink
       raise e
+    else
+      tempfile
     end
-    tempfile
   end
 
   ##
@@ -708,9 +729,16 @@ class Bitstream < ApplicationRecord
   # @param format [Symbol] Format extension with no leading dot.
   # @return [String]
   #
-  def derivative_key(region:, size:, format:)
+  def derivative_image_key(region:, size:, format:)
     [derivative_key_prefix, region.to_s, size.to_s,
      "default.#{format}"].join("/")
+  end
+
+  ##
+  # @return [String]
+  #
+  def derivative_pdf_key
+    [derivative_key_prefix, "pdf", "pdf.pdf"].join("/")
   end
 
   def derivative_key_prefix
@@ -719,7 +747,9 @@ class Bitstream < ApplicationRecord
 
   def ensure_primary_uniqueness
     if self.primary
-      self.item.bitstreams.where.not(id: self.id).update_all(primary: false)
+      self.item.bitstreams.
+        where.not(id: self.id).
+        update_all(primary: false)
     end
   end
 
@@ -731,46 +761,41 @@ class Bitstream < ApplicationRecord
   # @param size [Integer]
   # @param format [Symbol]
   #
-  def generate_derivative(region:, size:, format:)
-    target_key      = derivative_key(region: region, size: size, format: format)
+  def generate_image_derivative(region:, size:, format:)
+    target_key      = derivative_image_key(region: region,
+                                           size:   size,
+                                           format: format)
     source_tempfile = nil
     deriv_path      = nil
     begin
       Dir.mktmpdir do |tmpdir|
-        source_tempfile = download_to_temp_file
-        deriv_path      = File.join(tmpdir,
-                                    "#{File.basename(source_tempfile.path)}-#{region}-#{size}.#{format}")
         # N.B.: each case must write a file to deriv_path.
         case self.format.derivative_generator
         when "imagemagick"
-          crop       = (region == :square) ? "-gravity center -crop 1:1" : ""
-          command    = "convert #{source_tempfile.path}[0] "\
-                       "#{crop} "\
-                       "-resize #{size}x#{size} "\
-                       "-alpha remove "\
-                       "#{deriv_path}"
-          result     = system(command)
-          status     = $?.exitstatus
+          source_tempfile = download_to_temp_file
+          deriv_path      = File.join(tmpdir,
+                                      "#{File.basename(source_tempfile.path)}-#{region}-#{size}.#{format}")
+          crop            = (region == :square) ? "-gravity center -crop 1:1" : ""
+          command         = "convert #{source_tempfile.path}[0] "\
+                            "#{crop} "\
+                            "-resize #{size}x#{size} "\
+                            "-alpha remove "\
+                            "#{deriv_path}"
+          result          = system(command)
+          status          = $?.exitstatus
           raise "Command returned status code #{status}: #{command}" unless result
         when "libreoffice"
           # LibreOffice can convert to images itself, but it doesn't offer very
-          # much control over cropping, DPI, etc. Therefore we use it to convert
-          # to PDF and then convert that to our desired image using ImageMagick.
-          command  = "soffice --headless --convert-to pdf "\
-                     "#{source_tempfile.path} "\
-                     "--outdir #{tmpdir}"
-          result   = system(command)
-          status   = $?.exitstatus
-          raise "Command returned status code #{status}: #{command}" unless result
-
-          pdf_path = File.join(tmpdir,
-                               File.basename(source_tempfile.path).gsub(/#{File.extname(source_tempfile.path)}\z/, ".pdf"))
-          command = "convert #{pdf_path}[0] "\
-                    "#{crop} "\
-                    "-resize #{size}x#{size} "\
-                    "-background white "\
-                    "-alpha remove "\
-                    "#{deriv_path}"
+          # much control over cropping, DPI, etc. Therefore we use it to
+          # convert to PDF and then go from there with ImageMagick.
+          pdf_path   = generate_pdf_derivative(as_file: true)
+          deriv_path = "#{File.dirname(pdf_path)}/#{File.basename(pdf_path)}-#{region}-#{size}.#{format}"
+          command    = "convert #{pdf_path}[0] "\
+                       "#{crop} "\
+                       "-resize #{size}x#{size} "\
+                       "-background white "\
+                       "-alpha remove "\
+                       "#{deriv_path}"
           result  = system(command)
           status  = $?.exitstatus
           raise "Command returned status code #{status}: #{command}" unless result
@@ -782,11 +807,48 @@ class Bitstream < ApplicationRecord
         end
       end
     rescue => e
-      LOGGER.warn("generate_derivative(): #{e}")
+      LOGGER.warn("generate_image_derivative(): #{e}")
       raise e
     ensure
       source_tempfile&.unlink
       FileUtils.rm(deriv_path) rescue nil
+    end
+  end
+
+  ##
+  # Downloads the object into a temp file, writes a derivative PDF into another
+  # temp file, and saves it to the application S3 bucket. This is used for
+  # converting PDF-like formats (e.g. Microsoft Office) formats into PDFs.
+  #
+  # param as_file [Boolean] If true, the PDF is returned as a file path.
+  # @see generate_image_derivative
+  #
+  def generate_pdf_derivative(as_file: false)
+    if self.format.derivative_generator != "libreoffice"
+      raise "This instance cannot be converted to PDF."
+    end
+    source_tempfile = download_to_temp_file
+    begin
+      tmpdir          = File.dirname(source_tempfile)
+      command         = "soffice --headless --convert-to pdf "\
+        "#{source_tempfile.path} "\
+        "--outdir #{tmpdir}"
+      result   = system(command)
+      status   = $?.exitstatus
+      raise "Command returned status code #{status}: #{command}" unless result
+
+      pdf_path = File.join(tmpdir,
+                           File.basename(source_tempfile).gsub(/#{File.extname(source_tempfile)}\z/, ".pdf"))
+      if as_file
+        return pdf_path
+      else
+        File.open(pdf_path, "rb") do |file|
+          PersistentStore.instance.put_object(key:  derivative_pdf_key,
+                                              file: file)
+        end
+      end
+    ensure
+      source_tempfile.unlink
     end
   end
 

config/formats.yml
@@ -181,6 +181,7 @@ doc:
   category: document
   icon: file-word-o
   derivative_generator: libreoffice
+  viewer_method: pdfjs_viewer_for
 docx:
   short_name: Word
   long_name: Word Document
@@ -192,6 +193,7 @@ docx:
   category: document
   icon: file-word-o
   derivative_generator: libreoffice
+  viewer_method: pdfjs_viewer_for
 dvi:
   short_name: DVI
   long_name: Device Independent Format
@@ -615,6 +617,7 @@ odg:
   category: document
   icon: file-image-o
   derivative_generator: libreoffice
+  viewer_method: pdfjs_viewer_for
 odi:
   short_name: ODI
   long_name: OpenDocument Image
@@ -634,6 +637,7 @@ odm:
   category: document
   icon: file-o
   derivative_generator: libreoffice
+  viewer_method: pdfjs_viewer_for
 odp:
   short_name: ODP
   long_name: OpenDocument Presentation
@@ -644,6 +648,7 @@ odp:
   category: document
   icon: file-o
   derivative_generator: libreoffice
+  viewer_method: pdfjs_viewer_for
 ods:
   short_name: ODS
   long_name: OpenDocument Spreadsheet
@@ -664,6 +669,7 @@ odt:
   category: document
   icon: file-text-o
   derivative_generator: libreoffice
+  viewer_method: pdfjs_viewer_for
 ogg:
   short_name: Ogg
   long_name: Ogg Vorbis
@@ -795,6 +801,7 @@ ppt:
   category: document
   icon: file-powerpoint-o
   derivative_generator: libreoffice
+  viewer_method: pdfjs_viewer_for
 pptx:
   short_name: PowerPoint
   long_name: PowerPoint
@@ -806,6 +813,7 @@ pptx:
   category: document
   icon: file-powerpoint-o
   derivative_generator: libreoffice
+  viewer_method: pdfjs_viewer_for
 ps:
   short_name: PostScript
   long_name: PostScript
@@ -930,6 +938,7 @@ sdd:
   category: document
   icon: file-o
   derivative_generator: libreoffice
+  viewer_method: pdfjs_viewer_for
 sdm:
   short_name: StarMail
   long_name: StarMail
@@ -949,6 +958,7 @@ sda:
   category: document
   icon: file-o
   derivative_generator: libreoffice
+  viewer_method: pdfjs_viewer_for
 sdc:
   short_name: StarCalc
   long_name: StarCalc
@@ -968,6 +978,7 @@ sdp:
   category: document
   icon: file-o
   derivative_generator: libreoffice
+  viewer_method: pdfjs_viewer_for
 sds:
   short_name: StarChart
   long_name: StarChart
@@ -987,6 +998,7 @@ sdw:
   category: binary
   icon: file-o
   derivative_generator: libreoffice
+  viewer_method: pdfjs_viewer_for
 sgl:
   short_name: StarWriter
   long_name: StarWriter Global
@@ -1190,6 +1202,7 @@ vsd:
   category: document
   icon: file-o
   derivative_generator: libreoffice
+  viewer_method: pdfjs_viewer_for
 warc:
   short_name: WARC
   long_name: Web ARChive

test/controllers/bitstreams_controller_test.rb
@@ -109,7 +109,7 @@ class BitstreamsControllerTest < ActionDispatch::IntegrationTest
     fixture = file_fixture("pdf.pdf")
     item    = items(:southwest_unit1_collection1_item1)
     bs      = Bitstream.new_in_staging(item:     item,
-                                       filename: File.basename(fixture),
+                                       filename: "new_pdf.pdf",
                                        length:   File.size(fixture))
     bs.save!
     begin
@@ -128,7 +128,7 @@ class BitstreamsControllerTest < ActionDispatch::IntegrationTest
     fixture = file_fixture("pdf.pdf")
     item    = items(:southwest_unit1_collection1_item1)
     bs      = Bitstream.new_in_staging(item:     item,
-                                       filename: File.basename(fixture),
+                                       filename: "new_pdf.pdf",
                                        length:   File.size(fixture))
     bs.save!
     begin
@@ -232,7 +232,7 @@ class BitstreamsControllerTest < ActionDispatch::IntegrationTest
     fixture = file_fixture("pdf.pdf")
     item    = items(:southwest_unit1_collection1_item1)
     bs      = Bitstream.new_in_staging(item:     item,
-                                       filename: File.basename(fixture),
+                                       filename: "new_pdf.pdf",
                                        length:   File.size(fixture))
     bs.save!
     begin
@@ -462,7 +462,7 @@ class BitstreamsControllerTest < ActionDispatch::IntegrationTest
     fixture = file_fixture("pdf.pdf")
     item    = items(:southwest_unit1_collection1_item1)
     bs      = Bitstream.new_in_staging(item:     item,
-                                       filename: File.basename(fixture),
+                                       filename: "new_pdf.pdf",
                                        length:   File.size(fixture))
     bs.save!
     begin
@@ -481,7 +481,7 @@ class BitstreamsControllerTest < ActionDispatch::IntegrationTest
     fixture = file_fixture("pdf.pdf")
     item    = items(:southwest_unit1_collection1_item1)
     bs      = Bitstream.new_in_staging(item:     item,
-                                       filename: File.basename(fixture),
+                                       filename: "new_pdf.pdf",
                                        length:   File.size(fixture))
     bs.staging_key = nil
     bs.save!
@@ -494,7 +494,7 @@ class BitstreamsControllerTest < ActionDispatch::IntegrationTest
     fixture = file_fixture("pdf.pdf")
     item    = items(:southwest_unit1_collection1_item1)
     bs      = Bitstream.new_in_staging(item:     item,
-                                       filename: File.basename(fixture),
+                                       filename: "new_pdf.pdf",
                                        length:   File.size(fixture))
     bs.save!
     begin
@@ -514,7 +514,7 @@ class BitstreamsControllerTest < ActionDispatch::IntegrationTest
     fixture = file_fixture("pdf.pdf")
     item    = items(:southwest_unit1_collection1_item1)
     bs      = Bitstream.new_in_staging(item:     item,
-                                       filename: File.basename(fixture),
+                                       filename: "new_pdf.pdf",
                                        length:   File.size(fixture))
     bs.save!
     begin
@@ -645,6 +645,26 @@ class BitstreamsControllerTest < ActionDispatch::IntegrationTest
     ensure
       bs.delete_from_staging
     end
+  end
+
+  test "show() with pdf format returns HTTP 200 for a native PDF" do
+    bs = bitstreams(:southwest_unit1_collection1_item1_pdf)
+    get item_bitstream_path(bs.item, bs, format: :pdf)
+    assert_response :ok
+  end
+
+  test "show() with pdf format redirects for a bitstream that can be converted
+  into PDF" do
+    bs = bitstreams(:southwest_unit1_collection1_item1_doc)
+    get item_bitstream_path(bs.item, bs, format: :pdf)
+    assert_response :found
+  end
+
+  test "show() with pdf format returns HTTP 404 for a bitstream that cannot be
+  represented as PDF" do
+    bs = bitstreams(:southwest_unit1_collection1_item1_approved)
+    get item_bitstream_path(bs.item, bs, format: :pdf)
+    assert_response :not_found
   end
 
   test "show() respects role limits" do

test/fixtures/bitstreams.yml
@@ -37,6 +37,34 @@ southwest_unit1_collection1_item1_approved:
   bundle_position:   0
   description:       Sample description
 
+southwest_unit1_collection1_item1_doc:
+  item:              southwest_unit1_collection1_item1
+  staging_key:
+  permanent_key:     <%= SecureRandom.hex %> <-- will be updated by fix_bitstream_keys()
+  length:            <%= File.size(File.join(Rails.root, "test", "fixtures", "files", "IDEALS_Charter.doc")) %>
+  original_filename: IDEALS_Charter.doc
+  filename:          IDEALS_Charter.doc
+  medusa_key:
+  medusa_uuid:
+  role:              <%= Role::LOGGED_OUT %>
+  bundle:            <%= Bitstream::Bundle::CONTENT %>
+  bundle_position:   0
+  description:       Sample description
+
+southwest_unit1_collection1_item1_pdf:
+  item:              southwest_unit1_collection1_item1
+  staging_key:
+  permanent_key:     <%= SecureRandom.hex %> <-- will be updated by fix_bitstream_keys()
+  length:            <%= File.size(File.join(Rails.root, "test", "fixtures", "files", "pdf.pdf")) %>
+  original_filename: pdf.pdf
+  filename:          pdf.pdf
+  medusa_key:
+  medusa_uuid:
+  role:              <%= Role::LOGGED_OUT %>
+  bundle:            <%= Bitstream::Bundle::CONTENT %>
+  bundle_position:   0
+  description:       Sample description
+
 southwest_unit1_collection1_item1_license:
   item:              southwest_unit1_collection1_item1
   staging_key:

commit 2cfc316efab287ef68e0a4b6b4e55a03894b6b0c

Rewrite show() to fix failure to respond to XHR requests
Fixes a regression introduced in cdb99299844c78f7d09a5c42e87134b7660fb3ea

app/controllers/bitstreams_controller.rb
@@ -251,21 +251,18 @@ class BitstreamsController < ApplicationController
   # Responds to `GET /items/:item_id/bitstreams/:id`
   #
   def show
-    respond_to do |format|
-      format.pdf do
-        format = @bitstream.format
-        if format.media_type == "application/pdf"
-          data
-        elsif format.derivative_generator == "libreoffice"
-          redirect_to @bitstream.derivative_pdf_url, allow_other_host: true
-        else
-          render plain: "Unable to provide a PDF representation of this bitstream.",
-                 status: :not_found
-        end
+    if request.format == :pdf
+      format = @bitstream.format
+      if format.media_type == "application/pdf"
+        data
+      elsif format.derivative_generator == "libreoffice"
+        redirect_to @bitstream.derivative_pdf_url, allow_other_host: true
+      else
+        render plain: "Unable to provide a PDF representation of this bitstream.",
+               status: :not_found
       end
-      format.any do
-        render formats: :json
-      end
+    else
+      render formats: :json
     end
   end
 