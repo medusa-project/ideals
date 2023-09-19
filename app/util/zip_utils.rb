15b91cc078d6f1cb669a4b0a78270b3fcf714580
Alex Dolski <alexd@n-ary.net>
2023-09-19 13:20:23 -0500

    Support YYYY-MM-DD format dates in `unzip -l` output

--- app/util/zip_utils.rb
+++ app/util/zip_utils.rb
@@ -18,7 +18,7 @@ class ZipUtils
     # 31b90daad82cdd7412daea0972ed3df729db5095 (THIS LINE MAY BE MISSING)
     #   Length      Date    Time    Name
     # ---------  ---------- -----   ----
-    #         0  10-21-2015 15:26   stuff/
+    #         0  10-21-2015 15:26   stuff/     (N.B.: dates may be MM-DD-YYYY or YYYY-MM-DD)
     #      7568  10-21-2015 15:14   stuff/file1
     #       378  10-17-2015 22:32   stuff/file2
     # ---------                     -------
@@ -28,17 +28,20 @@ class ZipUtils
     lines        = lines[file1_offset..(lines.length - 3)]
     files        = lines.map(&:strip).
       reject{ |line| line.end_with?("/") }. # filter out directories
-      map { |line| line.match(/\A(\d+) +([\d-]+) (\d{2}:\d{2}) +(.+)\z/) }.
-      map { |match| {
+      map{ |line| line.match(/\A(\d+) +([\d-]+) (\d{2}:\d{2}) +(.+)\z/) }.
+      map{ |match| {
         name:   match[4],
         length: match[1].to_i,
         date:   "#{match[2]} #{match[3]}" } }.
       sort{ |a,b| a[:name] <=> b[:name] }
     # Transform the string dates into Time objects
     files.each do |f|
-      parts    = f[:date].match(/(\d+)-(\d+)-(\d+) (\d+):(\d+)/)
-      f[:date] = Time.new(parts[3].to_i, parts[1].to_i,
-                          parts[2].to_i, parts[4].to_i, parts[5].to_i)
+      parts = f[:date].match(/(\d+)-(\d+)-(\d+) (\d+):(\d+)/)
+      if parts[3].to_s.length == 4 # assume MM-DD-YYYY
+        f[:date] = Time.new(parts[3], parts[1], parts[2], parts[4], parts[5])
+      else # assume YYYY-MM-DD
+        f[:date] = Time.new(parts[1], parts[2], parts[3], parts[4], parts[5])
+      end
     end
     files
   end
