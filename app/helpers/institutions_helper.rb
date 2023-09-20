a66a0a9893ce730904ff014a61d47b58b669dc15
Alex Dolski <alexd@n-ary.net>
2023-09-20 10:47:06 -0500

    Move some redundant HTML into x509_cert_as_dl()

--- app/helpers/institutions_helper.rb
+++ app/helpers/institutions_helper.rb
@@ -30,4 +30,50 @@ module InstitutionsHelper
     end
   end
 
+  ##
+  # @param cert [String]
+  # @return [String] HTML <dl> element.
+  #
+  def x509_cert_as_dl(cert)
+    html = StringIO.new
+    cert = OpenSSL::X509::Certificate.new(cert)
+    html << "<div class=\"card\">"
+    html <<   "<div class=\"card-body\">"
+    html <<     "<dl>"
+    html <<       "<dt>Signature Algorithm</dt>"
+    html <<       "<dd>"
+    html <<         cert.signature_algorithm
+    if cert.signature_algorithm != "sha256WithRSAEncryption"
+      html << boolean(false, style: :word, false_value: "INVALID")
+    end
+    html <<       "</dd>"
+    html <<       "<dt>Subject</dt>"
+    html <<       "<dd>"
+    html <<         "<code style=\"word-break: break-all\">"
+    html <<           cert.subject
+    html <<         "</code>"
+    html <<       "</dd>"
+    html <<       "<dt>Issuer</dt>"
+    html <<       "<dd>"
+    html <<         "<code style=\"word-break: break-all\">"
+    html <<           cert.issuer
+    html <<         "</code>"
+    html <<       "</dd>"
+    html <<       "<dt>Issued</dt>"
+    html <<       "<dd>"
+    html <<         cert.not_before
+    html <<       "</dd>"
+    html <<       "<dt>Expires</dt>"
+    html <<       "<dd>"
+    html <<         cert.not_after
+    html <<       "</dd>"
+    html <<     "</dl>"
+    html <<   "</div>"
+    html << "</div>"
+    raw(html.string)
+  rescue
+    html = boolean(false, style: :word, false_value: "INVALID")
+    raw(html)
+  end
+
 end
