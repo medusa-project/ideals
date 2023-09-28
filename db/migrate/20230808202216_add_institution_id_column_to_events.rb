20226fe4c6d40e639cb5bcc0db19741fe8422bf1
Alex Dolski <alexd@n-ary.net>
2023-09-27 23:40:57 -0500

    All these updates are too slow so use a column default

--- db/migrate/20230808202216_add_institution_id_column_to_events.rb
+++ db/migrate/20230808202216_add_institution_id_column_to_events.rb
@@ -1,54 +1,8 @@
 class AddInstitutionIdColumnToEvents < ActiveRecord::Migration[7.0]
   def up
-    add_column :events, :institution_id, :bigint
+    add_column :events, :institution_id, :bigint, default: 1
     add_foreign_key :events, :institutions, on_update: :cascade, on_delete: :cascade
-
-    # Logins
-    results = execute("SELECT DISTINCT e.login_id, u.institution_id
-                       FROM events e
-                       LEFT JOIN logins l ON l.id = e.login_id
-                       LEFT JOIN users u ON l.user_id = u.id
-                       WHERE e.login_id IS NOT NULL;")
-    map = {}
-    results.each do |row|
-      map[row['institution_id']] = [] if map[row['institution_id']].nil?
-      map[row['institution_id']] << row['login_id']
-    end
-    map.each do |institution_id, login_ids|
-      execute("UPDATE events SET institution_id = #{institution_id}
-               WHERE login_id IN (#{login_ids.join(",")});") if institution_id
-    end
-
-    # Items
-    results = execute("SELECT DISTINCT e.item_id, i.institution_id
-                       FROM events e
-                       LEFT JOIN items i ON e.item_id = i.id
-                       WHERE e.item_id IS NOT NULL AND i.institution_id IS NOT NULL;")
-    map = {}
-    results.each do |row|
-      map[row['institution_id']] = [] if map[row['institution_id']].nil?
-      map[row['institution_id']] << row['item_id']
-    end
-    map.each do |institution_id, item_ids|
-      execute("UPDATE events SET institution_id = #{institution_id}
-               WHERE item_id IN (#{item_ids.join(",")});")
-    end
-
-    # Bitstreams
-    results = execute("SELECT DISTINCT e.bitstream_id, i.institution_id
-                       FROM events e
-                       LEFT JOIN bitstreams b ON e.bitstream_id = b.id
-                       LEFT JOIN items i ON b.item_id = i.id
-                       WHERE e.bitstream_id IS NOT NULL AND i.institution_id IS NOT NULL;")
-    map = {}
-    results.each do |row|
-      map[row['institution_id']] = [] if map[row['institution_id']].nil?
-      map[row['institution_id']] << row['bitstream_id']
-    end
-    map.each do |institution_id, bitstream_ids|
-      execute("UPDATE events SET institution_id = #{institution_id}
-               WHERE bitstream_id IN (#{bitstream_ids.join(",")});")
-    end
+    change_column_default :events, :institution_id, nil
 
     add_index :events, :institution_id
     add_index :events, :created_at
