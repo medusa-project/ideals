9b9429217fead6a2aa3d3e356375cc6d86d7fe85
Alex Dolski <alexd@n-ary.net>
2024-01-08 09:42:30 -0600

    Remove a[data-confirm] click handler

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -5,37 +5,6 @@ $(document).ready(function() {
     // an XHR success callback if there are any popovers loaded that way.
     IDEALS.UIUtils.enablePopovers();
 
-    // Reimplement the form element data-confirm functionality that used to be
-    // in rails-ujs, which is no longer available in Rails 7.
-    $("a[data-confirm]").on("click", function(e) {
-        e.preventDefault();
-        const elem   = $(this);
-        const result = confirm(elem.data("confirm"));
-        if (result) {
-            const form = $("<form method='post'></form>");
-            form.attr("action", elem.attr("href"));
-            var input = $("<input type='hidden'>");
-            input.attr("name", "_method");
-            input.attr("value", elem.data("method"));
-            form.append(input);
-            var input = $("<input type='hidden'>");
-            input.attr("name", "authenticity_token");
-            input.attr("value", $("meta[name=csrf-token]").attr("content"));
-            form.append(input);
-            $.each(this.attributes, function() {
-                if (this.name.startsWith("data-")) {
-                    const input = $("<input type='hidden'>");
-                    input.attr("name", this.name.substr(5));
-                    input.attr("value", this.value);
-                    form.append(input);
-                }
-            });
-            $("body").append(form);
-            form.trigger("submit");
-        }
-        return false;
-    });
-
     // When a modal is closed, clear its form.
     $(".modal").on("hidden.bs.modal", function() {
         $(this).find("input, select, textarea").not("input[type=submit]").val("");
