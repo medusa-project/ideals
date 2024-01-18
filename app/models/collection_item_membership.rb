1a3283ad2b710f90b661e3bbe574d0bc51186e0f
Alex Dolski <alexd@n-ary.net>
2024-01-18 10:54:19 -0600

    Add class documentation

--- app/models/collection_item_membership.rb
+++ app/models/collection_item_membership.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+##
+# Relates an {Item} to an owning {Collection}.
+#
+# # Attributes
+#
+# * `collection_id` Foreign key to {Collection}.
+# * `created_at`    Managed by ActiveRecord.
+# * `item_id`       Foreign key to {Item}.
+# * `primary`       Whether the membership is the item's primary collection
+#                   membership. (It can only have one.)
+# * `updated_at`    Managed by ActiveRecord.
+#
 class CollectionItemMembership < ApplicationRecord
   belongs_to :collection
   belongs_to :item
