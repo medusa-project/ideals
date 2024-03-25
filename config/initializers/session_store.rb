0396bb33bc840ebaf9078e79939be681ca3126e8
Alex Dolski <alexd@n-ary.net>
2024-03-25 15:04:16 -0500

    Set the session to expire after 1 day

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,3 @@
-Rails.application.config.session_store :active_record_store, key: "_ideals_session"
+Rails.application.config.session_store :active_record_store,
+                                       key: "_ideals_session",
+                                       expire_after: 1.day
