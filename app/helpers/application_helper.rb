medusa-project/ideals

commit df208bde2acfe8fc8e29dab47a5ead825de73f49

resource_list_row() displays the effective title

app/helpers/application_helper.rb
@@ -804,9 +804,9 @@ module ApplicationHelper
     html <<   "<div class=\"flex-grow-1 ms-3\">"
     html <<     "<h5 class=\"mt-0 mb-0\">"
     if embargoed_item && !policy(resource).show?
-      html <<     resource.title
+      html <<     resource.effective_title
     else
-      html <<     link_to(resource.title, resource_url)
+      html <<     link_to(resource.effective_title, resource_url)
     end
 
     if primary

commit 19ec0540527eb5ff15529cab46123d0a92a723e8

Fix a bug introduced in df208bde2acfe8fc8e29dab47a5ead825de73f49

app/helpers/application_helper.rb
@@ -804,9 +804,15 @@ module ApplicationHelper
     html <<   "<div class=\"flex-grow-1 ms-3\">"
     html <<     "<h5 class=\"mt-0 mb-0\">"
     if embargoed_item && !policy(resource).show?
-      html <<     resource.effective_title
+      if resource.respond_to?(:effective_title)
+        html << resource.effective_title
+      else
+        html << resource.title
+      end
+    elsif resource.respond_to?(:effective_title)
+      html << link_to(resource.effective_title, resource_url)
     else
-      html <<     link_to(resource.effective_title, resource_url)
+      html << link_to(resource.title, resource_url)
     end
 
     if primary